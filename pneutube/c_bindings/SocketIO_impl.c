3ae58aebb2b44ac5f6ff970ae7acda85c0bcdfee
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2016-12-02 16:22:46 +0100

    SocketIO: remove trailling whitespace in C file

--- pneutube/c_bindings/SocketIO_impl.c
+++ pneutube/c_bindings/SocketIO_impl.c
@@ -16,14 +16,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <arpa/inet.h> 
+#include <arpa/inet.h>
 #include "FileIO_Types.h"
 
 FileIO_Types_socket init_socket;
 
 FileIO_Types_sresult SocketIO_tcp_connect(char* host, int port, FileIO_Types_socket* sh) {
   int sockfd = 0, n = 0;
-  struct sockaddr_in serv_addr; 
+  struct sockaddr_in serv_addr;
   if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
       perror("Error : Could not create socket");
@@ -35,23 +35,23 @@ FileIO_Types_sresult SocketIO_tcp_connect(char* host, int port, FileIO_Types_soc
     perror("SocketError, DNS lookup on host failed");
     return FileIO_Types_sresult_SocketError;
   }
-  memset(&serv_addr, 0, sizeof(serv_addr)); 
+  memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(port); 
-  bcopy((char *)server->h_addr, 
+  serv_addr.sin_port = htons(port);
+  bcopy((char *)server->h_addr,
 	(char *)&serv_addr.sin_addr.s_addr, server->h_length);
   /*
   if(inet_pton(AF_INET, host, &serv_addr.sin_addr)<=0)
     {
       perror("inet_pton error occured");
       return SocketError;
-    } 
+    }
   */
   if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
       perror("Error : Connect Failed");
       return FileIO_Types_sresult_SocketError;
-    } 
+    }
   sh->socket_fd = sockfd;
   sh->sent_bytes = 0;
   sh->received_bytes = 0;
@@ -60,23 +60,23 @@ FileIO_Types_sresult SocketIO_tcp_connect(char* host, int port, FileIO_Types_soc
 
 FileIO_Types_sresult SocketIO_tcp_listen(int port, FileIO_Types_socket* sh) {
   int listenfd = 0;
-  struct sockaddr_in serv_addr; 
+  struct sockaddr_in serv_addr;
 
   if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
       perror("Error : Could not create socket");
       return FileIO_Types_sresult_SocketError;
-    } 
-  memset(&serv_addr, 0, sizeof(serv_addr)); 
+    }
+  memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htons(port); 
+  serv_addr.sin_port = htons(port);
 
   if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
     perror("bind");
     return FileIO_Types_sresult_SocketError;
   }
-    
+
   if (listen(listenfd, 10) < 0) {
     perror("listen");
     return FileIO_Types_sresult_SocketError;
@@ -87,12 +87,12 @@ FileIO_Types_sresult SocketIO_tcp_listen(int port, FileIO_Types_socket* sh) {
 
 FileIO_Types_sresult SocketIO_tcp_accept(FileIO_Types_socket* lh, FileIO_Types_socket* conn) {
   int sockfd = 0;
-  sockfd = accept(lh->socket_fd, (struct sockaddr*)NULL, NULL); 
+  sockfd = accept(lh->socket_fd, (struct sockaddr*)NULL, NULL);
   if( sockfd < 0)
     {
       perror("Error : accept Failed");
       return FileIO_Types_sresult_SocketError;
-    } 
+    }
   conn->socket_fd = sockfd;
   conn->sent_bytes = 0;
   conn->received_bytes = 0;
@@ -107,7 +107,7 @@ FileIO_Types_sresult SocketIO_tcp_write_all(FileIO_Types_socket* conn, uint8_t* 
   }
   conn->sent_bytes += len;
   return FileIO_Types_sresult_SocketOk;
-}    
+}
 
 
 FileIO_Types_sresult SocketIO_tcp_read_all(FileIO_Types_socket* conn, uint8_t* buf, int len) {
