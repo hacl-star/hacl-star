90617622abd88602e0812f737c19642c9258d2e4
Karthikeyan Bhargavan <karthik.bhargavan@gmail.com>
2017-01-13 12:25:44 +0100

    more curves

---
+++ test/curve25519-amd64-64/crypto_scalarmult.h
@@ -0,0 +1,11 @@
+
+#ifndef crypto_scalarmult_kummer_H
+#define crypto_scalarmult_kummer_H
+
+#define crypto_scalarmult_kummer_avx2int_SCALARBYTES 32
+#define crypto_scalarmult_kummer_avx2int_BYTES 48
+ 
+extern int crypto_scalarmult(unsigned char *,const unsigned char *,const unsigned char *);
+extern int crypto_scalarmult_base(unsigned char *,const unsigned char *);
+
+#endif
