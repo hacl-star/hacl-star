29aa26006bb6cd9c2d8220edb59b6b0538bc1855
Karthikeyan Bhargavan <karthik.bhargavan@gmail.com>
2017-02-07 15:04:08 +0100

    trying vectors

--- snapshots/hacl-c/test-salsa.c
+++ snapshots/hacl-c/test-salsa.c
@@ -377,8 +377,8 @@ int32_t perf_salsa() {
 
 int32_t main()
 {
-  //  int32_t res = test_salsa();
   int32_t res = exit_success;
+  //  res = test_salsa();
   if (res == exit_success) {
     res = perf_salsa();
   }
