52a4ae2b1dbc35f768406dc831ae980ca3f43244
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2017-02-08 16:37:22 +0100

    Remove exposed function from AEAD Chacha20 Poly1305 code

--- snapshots/hacl-c-experimental/aead_chacha20_poly1305.h
+++ snapshots/hacl-c-experimental/aead_chacha20_poly1305.h
@@ -1,8 +1,6 @@
 #include "Chacha20.h"
 #include "Poly1305_64.h"
 
-void blit(uint8_t* src, uint32_t len, uint8_t* dest, uint32_t pos);
-
 void poly1305_key_gen(uint8_t* otk, uint8_t* key, uint8_t* nonce);
 
 uint32_t hacl_aead_chacha20_poly1305_encrypt(uint8_t *plaintext,  uint32_t plaintext_len,
