2863b80c8f8c875188f237cb0be0adf770d731d6
Jonathan Protzenko <protz@microsoft.com>
2017-05-17 08:34:26 -0700

    Incremental build; temporary workaround

--- test/openssl-engine/test.sh
+++ test/openssl-engine/test.sh
@@ -24,9 +24,6 @@ echo Running with engine: $ENGINE
 tput sgr0
 $OPENSSL_HOME/apps/openssl$EXE <<EOF
   engine $ENGINE
-  speed -engine Everest ecdhx25519
   speed -engine Everest -evp sha512
   speed -engine Everest -evp chacha20
-  speed -engine Everest -evp chacha20-poly1305
-  speed -engine Everest -evp poly1305
 EOF
