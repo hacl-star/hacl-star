e0656ffb0900ad6659f961579235f5f3fd0b26d2
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2017-10-18 16:06:45 +0200

    Refresh KreMLin library

--- snapshots/kremlib/gcc_compat.h
+++ snapshots/kremlib/gcc_compat.h
@@ -2,17 +2,17 @@
 #define __GCC_COMPAT_H
 
 #ifndef _MSC_VER
-  // Use the gcc predefined macros if on a platform/architectures that set them.  Otherwise define them to be empty.
-  #ifndef __cdecl
-    #define __cdecl
-  #endif
-  #ifndef __stdcall
-    #define __stdcall
-  #endif
-  #ifndef __fastcall
-    #define __fastcall
-  #endif
+/* Use the gcc predefined macros if on a platform/architectures that set them.
+ * Otherwise define them to be empty. */
+#ifndef __cdecl
+#define __cdecl
+#endif
+#ifndef __stdcall
+#define __stdcall
+#endif
+#ifndef __fastcall
+#define __fastcall
+#endif
 #endif
 
-
-#endif // __GCC_COMPAT_H
+#endif /* __GCC_COMPAT_H */
