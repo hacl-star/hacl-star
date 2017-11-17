dd58e33dd5e012edd837f79ff652726e27301c32
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2017-11-17 14:58:26 +0100

    Remove unnecessary patches for NSS

--- code/nss-patches/patch.sh
+++ code/nss-patches/patch.sh
@@ -2,17 +2,11 @@
 
 cwd=$(cd $(dirname $0); pwd -P)
 
-# Add license headers to all files.
-for file in `find "$cwd/../../snapshots/nss/" -type f`; do
-    cat "$cwd/license.txt" "$file" > "$file.tmp" && mv "$file.tmp" "$file"
-done
-
 # Patch files where file names changed.
-patch "$cwd/../../snapshots/nss/hacl_curve25519_64.h" < "$cwd/hacl_curve25519_64.h.patch"
 patch "$cwd/../../snapshots/nss/hacl_curve25519_64.c" < "$cwd/hacl_curve25519_64.c.patch"
 
 # Get snapshots/kremlib/gcc_compat.h into kremlib.h
 gcc_compat=$(cat "$cwd/../../snapshots/kremlib/gcc_compat.h")
-sed -i "/.*gcc_compat.h.*/r$cwd/../../snapshots/kremlib/gcc_compat.h" "$cwd/../../snapshots/nss/kremlib.h"
-sed -i '/gcc_compat.h/d' "$cwd/../../snapshots/nss/kremlib.h"
-sed -i '/__GCC_COMPAT_H/d' "$cwd/../../snapshots/nss/kremlib.h"
+$(SED) -i "/.*gcc_compat.h.*/r$cwd/../../snapshots/kremlib/gcc_compat.h" "$cwd/../../snapshots/nss/kremlib.h"
+$(SED) -i '/gcc_compat.h/d' "$cwd/../../snapshots/nss/kremlib.h"
+$(SED) -i '/__GCC_COMPAT_H/d' "$cwd/../../snapshots/nss/kremlib.h"
