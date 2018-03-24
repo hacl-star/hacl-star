3962d84df79661d9fa4f9c7e29f01cce756508d2
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2018-03-24 10:52:17 +0100

    Print and compare IntBuffers

--- specs/lib/c/Spec_Lib_Print.c
+++ specs/lib/c/Spec_Lib_Print.c
@@ -19,3 +19,18 @@ void Spec_Lib_Print_print_compare(uint32_t len, uint8_t* buffer1, uint8_t* buffe
   }
   printf("\n");
 }
+
+void Spec_Lib_Print_print_compare_display(uint32_t len, uint8_t* buffer1, uint8_t* buffer2) {
+  Spec_Lib_Print_print_compare(len, buffer1, buffer2);
+  int res = 1;
+  for (int i = 0; i < len; i++) {
+    res |= buffer1[i] ^ buffer2[i];
+  }
+  if (res) {
+    printf("Success !\n");
+  } else {
+    printf("Failure !\n");
+  }
+  printf("\n");
+}
+
