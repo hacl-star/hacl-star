afcb792e27a271a6c2f8dc3246eb250ee668f3bc
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2018-04-14 22:34:35 +0200

    Move haclnacl.c/h to use inttypes.h

--- snapshots/api/haclnacl.c
+++ snapshots/api/haclnacl.c
@@ -76,55 +76,55 @@ void sha2_512_hash(uint8_t *hash, uint8_t *input, uint32_t len){
 
 /* NaCl-like API */
 
-int crypto_auth(unsigned char *output, const unsigned char *input, unsigned long long input_len, const unsigned char *key){
-  Hacl_HMAC_SHA2_256_hmac_core(output, (unsigned char *)key, (unsigned char *)input, input_len);
+int crypto_auth(uint8_t *output, const uint8_t *input, uint64_t input_len, const uint8_t *key){
+  Hacl_HMAC_SHA2_256_hmac_core(output, (uint8_t *)key, (uint8_t *)input, input_len);
   return 0;
 }
 
-int crypto_auth_verify(const unsigned char *tag, const unsigned char *input, unsigned long long input_len, const unsigned char *key){
+int crypto_auth_verify(const uint8_t *tag, const uint8_t *input, uint64_t input_len, const uint8_t *key){
   uint8_t recomputed_tag[32], tmp = 0xff;
-  Hacl_HMAC_SHA2_256_hmac_core(recomputed_tag, (unsigned char *)key, (unsigned char *)input, input_len);
+  Hacl_HMAC_SHA2_256_hmac_core(recomputed_tag, (uint8_t *)key, (uint8_t *)input, input_len);
   for (int i = 0; i < 32; i++){
-    tmp = tmp & FStar_UInt8_eq_mask((unsigned char)tag[i], recomputed_tag[i]);
+    tmp = tmp & FStar_UInt8_eq_mask((uint8_t)tag[i], recomputed_tag[i]);
   }
   tmp >>= 7;
   return (int)tmp - 1;
 }
 
 
-int crypto_box_keypair(unsigned char *pk, unsigned char *sk){
+int crypto_box_keypair(uint8_t *pk, uint8_t *sk){
   randombytes(sk, 32);
   uint8_t basepoint[32] = {9};
   curve25519_scalarmult(pk, sk, basepoint);
   return 0;
 }
 
-int crypto_box_easy(unsigned char *c, const unsigned char *m,
-                    unsigned long long mlen, const unsigned char *n,
-                    const unsigned char *pk, const unsigned char *sk){
+int crypto_box_easy(uint8_t *c, const uint8_t *m,
+                    uint64_t mlen, const uint8_t *n,
+                    const uint8_t *pk, const uint8_t *sk){
   return NaCl_crypto_box_easy(c, (uint8_t*)m, mlen, (uint8_t*)n, (uint8_t*)pk, (uint8_t*)sk);
 }
 
-int crypto_box_open_easy(unsigned char *m, const unsigned char *c,
-                         unsigned long long clen, const unsigned char *n,
-                         const unsigned char *pk, const unsigned char *sk){
+int crypto_box_open_easy(uint8_t *m, const uint8_t *c,
+                         uint64_t clen, const uint8_t *n,
+                         const uint8_t *pk, const uint8_t *sk){
   return NaCl_crypto_box_open_easy(m, (uint8_t*)c, clen, (uint8_t*)n, (uint8_t*)pk, (uint8_t*)sk);
 }
 
-int crypto_box_beforenm(unsigned char *k, const unsigned char *pk,
-                        const unsigned char *sk){
+int crypto_box_beforenm(uint8_t *k, const uint8_t *pk,
+                        const uint8_t *sk){
   return NaCl_crypto_box_beforenm(k, (uint8_t*)pk, (uint8_t*)sk);
 }
 
-int crypto_box_easy_afternm(unsigned char *c, const unsigned char *m,
-                            unsigned long long mlen, const unsigned char *n,
-                            const unsigned char *k){
+int crypto_box_easy_afternm(uint8_t *c, const uint8_t *m,
+                            uint64_t mlen, const uint8_t *n,
+                            const uint8_t *k){
   return NaCl_crypto_box_easy_afternm(c, (uint8_t*)m, mlen, (uint8_t*)n, (uint8_t*)k);
 }
 
-int crypto_box_open_easy_afternm(unsigned char *m, const unsigned char *c,
-                                 unsigned long long clen, const unsigned char *n,
-                                 const unsigned char *k){
+int crypto_box_open_easy_afternm(uint8_t *m, const uint8_t *c,
+                                 uint64_t clen, const uint8_t *n,
+                                 const uint8_t *k){
   return NaCl_crypto_box_open_easy_afternm(m, (uint8_t*)c, clen, (uint8_t*)n, (uint8_t*)k);
 }
 
@@ -145,52 +145,52 @@ uint32_t crypto_box_open_detached_afternm(uint8_t *m, uint8_t *c, uint8_t *mac, 
 }
 
 
-int crypto_box(unsigned char *cipher, const unsigned char *msg, unsigned long long msg_len, const unsigned char *nonce,  const unsigned char *pk, const unsigned char *sk){
-  return crypto_box_easy(cipher, (unsigned char *)msg, msg_len - 32, (unsigned char *)nonce, (unsigned char *)pk, (unsigned char *)sk);
+int crypto_box(uint8_t *cipher, const uint8_t *msg, uint64_t msg_len, const uint8_t *nonce,  const uint8_t *pk, const uint8_t *sk){
+  return crypto_box_easy(cipher, (uint8_t *)msg, msg_len - 32, (uint8_t *)nonce, (uint8_t *)pk, (uint8_t *)sk);
 }
 
 int crypto_box_open(uint8_t *msg, const uint8_t *cipher, uint64_t cipher_len, const uint8_t *nonce, const uint8_t *pk, const uint8_t *sk){
   return crypto_box_open_easy(msg, cipher, cipher_len - 32, nonce, pk, sk);
 }
 
-int crypto_box_afternm(unsigned char *cipher, const unsigned char *msg, unsigned long long msg_len, const unsigned char *nonce, const uint8_t *key){
-  return NaCl_crypto_box_easy_afternm(cipher, (unsigned char *)msg, msg_len, (unsigned char *)nonce, (unsigned char *)key);
+int crypto_box_afternm(uint8_t *cipher, const uint8_t *msg, uint64_t msg_len, const uint8_t *nonce, const uint8_t *key){
+  return NaCl_crypto_box_easy_afternm(cipher, (uint8_t *)msg, msg_len, (uint8_t *)nonce, (uint8_t *)key);
 }
 
-int crypto_box_open_afternm(unsigned char *msg, const unsigned char *cipher, unsigned long long cipher_len, const unsigned char *nonce, const unsigned char *key){
-  return NaCl_crypto_box_open_easy_afternm(msg, (unsigned char *)cipher, cipher_len, (unsigned char *)nonce, (unsigned char *)key);
+int crypto_box_open_afternm(uint8_t *msg, const uint8_t *cipher, uint64_t cipher_len, const uint8_t *nonce, const uint8_t *key){
+  return NaCl_crypto_box_open_easy_afternm(msg, (uint8_t *)cipher, cipher_len, (uint8_t *)nonce, (uint8_t *)key);
 }
 
 
-int crypto_hash(unsigned char *output, const unsigned char *input,unsigned long long input_len){
-  Hacl_SHA2_256_hash(output, (unsigned char *)input, input_len);
+int crypto_hash(uint8_t *output, const uint8_t *input,uint64_t input_len){
+  Hacl_SHA2_256_hash(output, (uint8_t *)input, input_len);
   return 0;
 }
 
-int crypto_onetimeauth(unsigned char *output, const unsigned char *input, unsigned long long input_len, const unsigned char *key){
-  poly1305_onetimeauth(output, (unsigned char *)input, input_len, (unsigned char *)key);
+int crypto_onetimeauth(uint8_t *output, const uint8_t *input, uint64_t input_len, const uint8_t *key){
+  poly1305_onetimeauth(output, (uint8_t *)input, input_len, (uint8_t *)key);
   return 0;
 }
 
-int crypto_onetimeauth_verify(const unsigned char *auth, const unsigned char *input, unsigned long long input_len, const unsigned char *key){
+int crypto_onetimeauth_verify(const uint8_t *auth, const uint8_t *input, uint64_t input_len, const uint8_t *key){
   uint8_t tag[16], tmp = 0xff;
-  poly1305_onetimeauth(tag, (unsigned char *)input, input_len, (unsigned char *)key);
+  poly1305_onetimeauth(tag, (uint8_t *)input, input_len, (uint8_t *)key);
   for (int i = 0; i < 16; i++){
-    tmp = tmp & FStar_UInt8_eq_mask(tag[i], (unsigned char)auth[i]);
+    tmp = tmp & FStar_UInt8_eq_mask(tag[i], (uint8_t)auth[i]);
   }
   tmp >>= 7;
   return (int)tmp - 1;
 }
 
 
-int crypto_scalarmult_base(unsigned char *q, const unsigned char *n){
+int crypto_scalarmult_base(uint8_t *q, const uint8_t *n){
   /* This leaves room for improvements with precomputations */
   uint8_t basepoint[32] = {9};
   Hacl_Curve25519_crypto_scalarmult(q, (uint8_t*)n, basepoint);
   return 0;
 }
 
-int crypto_scalarmult(unsigned char *q, const unsigned char *n, const unsigned char *p){
+int crypto_scalarmult(uint8_t *q, const uint8_t *n, const uint8_t *p){
   Hacl_Curve25519_crypto_scalarmult(q, (uint8_t*)n, (uint8_t*)p);
   return 0;
 }
@@ -212,25 +212,25 @@ uint32_t crypto_secretbox_open_easy(uint8_t *m, uint8_t *c, uint64_t clen, uint8
   return NaCl_crypto_secretbox_open_easy(m, c, clen, n, k);
 }
 
-int crypto_secretbox(unsigned char *cipher, const unsigned char *msg, unsigned long long msg_len, const unsigned char *nonce, const unsigned char *key){
-  return crypto_secretbox_easy(cipher, (unsigned char *)msg, msg_len - 32, (unsigned char *)nonce, (unsigned char *)key);
+int crypto_secretbox(uint8_t *cipher, const uint8_t *msg, uint64_t msg_len, const uint8_t *nonce, const uint8_t *key){
+  return crypto_secretbox_easy(cipher, (uint8_t *)msg, msg_len - 32, (uint8_t *)nonce, (uint8_t *)key);
 }
 
-int crypto_secretbox_open(unsigned char *msg, const unsigned char *cipher, unsigned long long cipher_len, const unsigned char *nonce, const unsigned char *key){
-  return crypto_secretbox_open_detached(msg, (unsigned char *)cipher, (unsigned char *)cipher + 16, cipher_len - 32, (unsigned char *)nonce, (unsigned char *)key);
+int crypto_secretbox_open(uint8_t *msg, const uint8_t *cipher, uint64_t cipher_len, const uint8_t *nonce, const uint8_t *key){
+  return crypto_secretbox_open_detached(msg, (uint8_t *)cipher, (uint8_t *)cipher + 16, cipher_len - 32, (uint8_t *)nonce, (uint8_t *)key);
 }
 
 
-int crypto_sign(unsigned char *signed_msg, unsigned long long *signed_len, const unsigned char *msg, unsigned long long msg_len, const unsigned char *sk){
-  Hacl_Ed25519_sign(signed_msg, (unsigned char *)sk, (unsigned char *)msg, msg_len);
+int crypto_sign(uint8_t *signed_msg, uint64_t *signed_len, const uint8_t *msg, uint64_t msg_len, const uint8_t *sk){
+  Hacl_Ed25519_sign(signed_msg, (uint8_t *)sk, (uint8_t *)msg, msg_len);
   memmove(signed_msg+64, msg, msg_len * sizeof(uint8_t));
   *signed_len = msg_len + 64;
   return 0;
 }
 
-int crypto_sign_open(unsigned char *unsigned_msg, unsigned long long *unsigned_msg_len, const unsigned char *msg, unsigned long long msg_len, const unsigned char *pk){
+int crypto_sign_open(uint8_t *unsigned_msg, uint64_t *unsigned_msg_len, const uint8_t *msg, uint64_t msg_len, const uint8_t *pk){
   uint32_t res;
-  res = Hacl_Ed25519_verify((unsigned char *)pk, (unsigned char *)msg+64, msg_len - 64, (unsigned char *)msg);
+  res = Hacl_Ed25519_verify((uint8_t *)pk, (uint8_t *)msg+64, msg_len - 64, (uint8_t *)msg);
   if (res == true){
     memmove(unsigned_msg, msg+64, sizeof(uint8_t) * (msg_len-64));
     *unsigned_msg_len = msg_len - 64;
@@ -253,18 +253,18 @@ int crypto_sign_secret_to_public(uint8_t *public_key, uint8_t *secret_key){
 }
 
 
-int crypto_stream(unsigned char *cipher, unsigned long long cipher_len, const unsigned char *nonce, const unsigned char *key){
+int crypto_stream(uint8_t *cipher, uint64_t cipher_len, const uint8_t *nonce, const uint8_t *key){
   uint8_t subkey[32];
   memset(cipher, 0, cipher_len * sizeof(uint8_t));
-  Hacl_Salsa20_hsalsa20(subkey, (unsigned char *)key, (unsigned char *)nonce);
-  Hacl_Salsa20_salsa20(cipher, cipher, cipher_len, subkey, (unsigned char *)nonce + 16, 0);
+  Hacl_Salsa20_hsalsa20(subkey, (uint8_t *)key, (uint8_t *)nonce);
+  Hacl_Salsa20_salsa20(cipher, cipher, cipher_len, subkey, (uint8_t *)nonce + 16, 0);
   return 0;
 }
 
-int crypto_stream_xor(unsigned char *cipher, const unsigned char *msg, unsigned long long cipher_len, const unsigned char *nonce, const unsigned char *key){
+int crypto_stream_xor(uint8_t *cipher, const uint8_t *msg, uint64_t cipher_len, const uint8_t *nonce, const uint8_t *key){
   uint8_t subkey[32];
   memset(cipher, 0, cipher_len * sizeof(uint8_t));
-  Hacl_Salsa20_hsalsa20(subkey, (unsigned char *)key, (unsigned char *)nonce);
-  Hacl_Salsa20_salsa20(cipher, (unsigned char *)msg, cipher_len, subkey, (unsigned char *)nonce + 16, 0);
+  Hacl_Salsa20_hsalsa20(subkey, (uint8_t *)key, (uint8_t *)nonce);
+  Hacl_Salsa20_salsa20(cipher, (uint8_t *)msg, cipher_len, subkey, (uint8_t *)nonce + 16, 0);
   return 0;
 }
