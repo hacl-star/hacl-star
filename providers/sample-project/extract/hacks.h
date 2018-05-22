50db1fd158aa504584be812248fbe9d85846da15
Santiago Zanella-Beguelin <santiago@microsoft.com>
2018-05-22 17:48:21 +0100

    Add tests for EverCrypt and revamp Makefile

---
+++ providers/sample-project/extract/hacks.h
@@ -0,0 +1,32 @@
+#include "kremlib.h"
+#ifdef __EverCrypt_H_DEFINED
+#ifndef __Hacks_H
+#define __Hacks_H
+
+#include "Prims.h"
+#include "FStar.h"
+#include "kremlin/prims_int.h"
+#include "kremlin/prims_string.h"
+
+typedef struct K___uint8_t_uint8_t_s
+{
+  uint8_t fst;
+  uint8_t snd;
+}
+K___uint8_t_uint8_t;
+
+typedef FStar_Pervasives_Native_option__EverCrypt_Native_impl_tags
+FStar_Pervasives_Native_option__Prims_string_tags;
+
+typedef struct FStar_Pervasives_Native_option__Prims_string_s
+{
+  FStar_Pervasives_Native_option__Prims_string_tags tag;
+  Prims_string v;
+}
+FStar_Pervasives_Native_option__Prims_string;
+
+#include "kremlin/fstar_bytes.h"
+
+#define __Hacks_H_DEFINED
+#endif
+#endif
