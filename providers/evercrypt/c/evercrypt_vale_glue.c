6b77d8717266c45642ec3096b7b6533bcd79ad28
Barry Bond <barrybo@microsoft.com>
2018-06-07 11:09:51 -0700

    Add stubs for Vale assembler files, that are portable to other architectures, to satisfy unresolved externs.

--- providers/evercrypt/c/evercrypt_vale_glue.c
+++ providers/evercrypt/c/evercrypt_vale_glue.c
@@ -46,3 +46,56 @@ void EverCrypt_ValeGlue_sha256_hash(uint8_t *x0, uint8_t *x1, uint32_t x2){
   KRML_HOST_EPRINTF("TODO: sha256_hash/Vale\n");
   KRML_HOST_EXIT(255);
 }
+
+#if !defined(_M_X64) && !defined(__x86_64__)
+// On non-x64 builds, define no-op stubs for Vale assembly code
+void __stdcall gcm_encrypt(void *x0)
+{
+    KRML_HOST_EPRINTF("Vale gcm_encrypt() isn't implemented in this platform.  Do not call.\n");
+    KRML_HOST_EXIT(255);
+}
+
+void
+__stdcall
+sha256_main_i_SHA256_Compute64Steps(
+  uint32_t *x0,
+  uint32_t *x1,
+  uint32_t x2,
+  uint32_t x3,
+  uint32_t x4,
+  uint32_t x5,
+  uint32_t x6,
+  uint32_t x7,
+  uint32_t x8,
+  uint32_t x9,
+  uint32_t x10,
+  uint32_t x11,
+  uint32_t x12,
+  uint32_t x13,
+  uint32_t x14,
+  uint32_t x15,
+  uint32_t x16,
+  uint32_t x17,
+  uint32_t x18
+)
+{
+    KRML_HOST_EPRINTF("Vale sha256_main_i_SHA256_Compute64Steps() isn't implemented in this platform.  Do not call.\n");
+    KRML_HOST_EXIT(255);
+}
+
+void
+__stdcall
+sha256_main_i_SHA256_ComputeInitialWs(
+  uint8_t *x0,
+  uint32_t x1,
+  uint32_t *x2,
+  uint32_t x3,
+  uint32_t x4,
+  uint32_t x5,
+  uint32_t x6
+)
+{
+    KRML_HOST_EPRINTF("Vale sha256_main_i_SHA256_Compute64Steps() isn't implemented in this platform.  Do not call.\n");
+    KRML_HOST_EXIT(255);
+}
+#endif
