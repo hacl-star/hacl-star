b2e8c74d101dfe05e6ee72e9216831b8dd14ef43
karthikbhargavan <karthik.bhargavan@gmail.com>
2018-06-09 09:29:35 +0200

    cleanup

--- specs/lib/c/Spec.Lib.Loops.fst
+++
@@ -1,9 +0,0 @@
-module Spec.Lib.Loops
-
-open FStar.HyperStack
-open FStar.HyperStack.ST
-open Spec.Lib.IntTypes
-
-let for start finish inv f = C.Loops.for start finish inv f
-
-
--- specs/lib/c/Spec_Lib_Print.h
+++
@@ -1,5 +0,0 @@
-#include "stdint.h"
-#include "stdio.h"
-
-void Spec_Lib_Print_print_bytes(uint32_t len, uint8_t* buffer);
-void Spec_Lib_Print_print_compare(uint32_t len, uint8_t* buffer1, uint8_t* buffer2);
