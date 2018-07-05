0461580ee9aa75705dd852760d65129ffb51b6d6
Santiago Zanella-Beguelin <santiago@microsoft.com>
2018-07-05 11:44:08 +0100

    Fix Hacl.Blake2 (swapped arguments) and Kremlin extraction of the test

--- lib/c/Lib_Print.h
+++ lib/c/Lib_Print.h
@@ -1,5 +1,6 @@
 #include "stdint.h"
 #include "stdio.h"
 
-void Spec_Lib_Print_print_bytes(uint32_t len, uint8_t* buffer);
-void Spec_Lib_Print_print_compare(uint32_t len, uint8_t* buffer1, uint8_t* buffer2);
+void Lib_Print_print_bytes(uint32_t len, uint8_t* buffer);
+void Lib_Print_print_compare(uint32_t len, uint8_t* buffer1, uint8_t* buffer2);
+void Lib_Print_print_compare_display(uint32_t len, uint8_t* buffer1, uint8_t* buffer2);
