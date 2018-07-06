fb3d284fea653f39d0d8836f07fb6464ebb98524
Santiago Zanella-Beguelin <santiago@microsoft.com>
2018-07-06 17:34:39 +0100

    WIP

--- code/frodo/include/Hacl_Keccak.h
+++ code/frodo/include/Hacl_Keccak.h
@@ -1,4 +1,9 @@
+#ifndef __Hacl_Keccak_H
+#define __Hacl_Keccak_H
+
 #include "fips202.h"
 
 void Hacl_Keccak_cshake128_frodo(uint32_t x0, uint8_t *x1, uint16_t x2, uint32_t x3, uint8_t *x4);
 void Hacl_Keccak_cshake256_frodo(uint32_t x0, uint8_t *x1, uint16_t x2, uint32_t x3, uint8_t *x4);
+
+#endif
