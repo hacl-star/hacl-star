882cf6fb2fc293f344b654591aacef2b70d5ab9d
Marina Polubelova <polubelovam@gmail.com>
2018-07-06 17:08:56 +0100

    wip: add keccak

---
+++ code/frodo/include/Hacl_Keccak.h
@@ -0,0 +1,4 @@
+#include "fips202.h"
+
+void Hacl_Keccak_cshake128_frodo(uint32_t x0, uint8_t *x1, uint16_t x2, uint32_t x3, uint8_t *x4);
+void Hacl_Keccak_cshake256_frodo(uint32_t x0, uint8_t *x1, uint16_t x2, uint32_t x3, uint8_t *x4);
