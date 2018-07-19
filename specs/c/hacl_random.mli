b2e583433fbea2adc4f124e24dfc953400405e0c
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2018-07-19 16:21:53 -0700

    OCaml stubs for Hacl_Unverified_Random.h/c

---
+++ specs/c/hacl_random.mli
@@ -0,0 +1,7 @@
+module Rand : sig
+  val write : Bytes.t -> unit
+  (** [write buf] writes random bytes on [buf]. *)
+
+  val gen : int -> Bytes.t
+  (** [gen len] is a random buffer of length [len]. *)
+end
