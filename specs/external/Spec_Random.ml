c467db61bf242b8d43d404a249b3e116e69c47c6
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2018-07-26 16:29:18 -0700

    Progress on having Spec.Random work

--- specs/external/Spec_Random.ml
+++ specs/external/Spec_Random.ml
@@ -2,10 +2,11 @@ open Cryptokit
 
 
 let generate len =
-  let buf = Bytes.create len in
+  let buf = Bytes.create (Z.to_int len) in
   let rng = Random.system_rng () in
-  rng#random_bytes buf 0 len
+  rng#random_bytes buf 0 (Z.to_int len);
+  buf
 
 let write len buf =
   let rng = Random.system_rng () in
-  rng#random_bytes buf 0 len
+  rng#random_bytes buf 0 (Z.to_int len)
