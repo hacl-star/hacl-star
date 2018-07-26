47cf1008378d7b00b7ee886c5217e285ef106db0
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2018-07-26 15:01:10 -0700

    Remove unused external functions for Random and AES-GCM

--- specs/external/Spec.Random.fst
+++
@@ -1,8 +0,0 @@
-module Spec.Random
-
-open Lib.IntTypes
-open Lib.ByteSequence
-
-assume val write: len:size_nat -> lbytes len -> Tot unit
-assume val gen: len:size_nat -> Tot (lbytes len)
-
--- specs/external/Spec_External.ml
+++
@@ -1,8 +0,0 @@
-open Bytes
-open Nocrypto
-
-val aes_gcm_encrypt: key:bytes -> iv:bytes -> ad:bytes -> input:bytes -> output:bytes
-let aes_gcm_encrypt key iv ad input = AES.GCM.encrypt key iv ad input
-
-val aes_gcm_decrypt: key:bytes -> iv:bytes -> ad:bytes -> input:bytes -> output:bytes
-let aes_gcm_decrypt key iv ad input = AES.GCM.decrypt key iv ad input
