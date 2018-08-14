fea2d47227374c3d88c8d00b540ab366466e3ad7
Santiago Zanella-Beguelin <santiago@microsoft.com>
2018-08-14 15:20:52 +0100

    Checking-in a copy of NIST submission with additional Makefile targets for testing F* code

---
+++ frodo/NIST/Optimized_Implementation/optimized/FrodoKEM-640/tests/test_KEM640.c
@@ -0,0 +1,17 @@
+/********************************************************************************************
+* FrodoKEM: Learning with Errors Key Encapsulation
+*
+* Abstract: setting parameters to test FrodoKEM-640
+*********************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "ds_benchmark.h"
+#include "../api.h"
+
+
+#define SYSTEM_NAME    "FrodoKEM-640"
+
+#include "test_kem.c"
