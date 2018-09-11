e8cce77bc560dcc5878972b617aa341d5d9c354e
Joonwon Choi <joonwonc@mit.edu>
2018-09-11 18:05:08 +0100

    Some changes in Merkle tree interface

--- secure_api/merkle_tree/merkle_tree_prototype.h
+++ secure_api/merkle_tree/merkle_tree_prototype.h
@@ -1,37 +1,40 @@
 #include "stdint.h"
 
-// TODO: should instantiate all `extern`s
-extern hash_t;
+extern hash_p;
 extern merkle_tree;
-extern signed_hash;
 
-typedef merkle_tree *mt_ptr;
+typedef merkle_tree *mt_p;
 
 /// Construction and destruction (free)
-mt_ptr create_mt(hash_t init);
-void free_mt(mt_ptr mt);
+mt_p create_mt(hash_p init);
+void free_mt(mt_p mt);
 
-/// Insertion
-void mt_insert(mt_ptr mt, hash_t v);
+/* Insertion
+ * @param[in] v NOTE that the content of v will be changed to an arbitrary
+ *              value after calling it.
+ */
+void mt_insert(mt_p mt, hash_p v);
 
 /** Getting the Merkle root
  * @param[out] root The merkle root returned as a hash pointer
  */
-void mt_get_root(mt_ptr mt, hash_t root);
+void mt_get_root(mt_p mt, hash_p root);
 
 /** Getting the Merkle path
  * @param idx The index of the target hash
- * @param[out] root A signed root by the server
- * @param[out] path A resulting Merkle path that contains the leaf hash
- * @return The maximum index + 1. This number represents the depth of `path`.
+ * @param[out] root The Merkle root
+ * @param[out] path A resulting Merkle path that contains the leaf hash.
+ *                  NOTE that the path contains pointers to hashes in the tree,
+ *                  not the actual hash values.
+ * @return The number of elements in the tree
  */
-uint32_t mt_get_path(mt_ptr mt, uint32_t idx, signed_hash root, hash_t *path);
+uint32_t mt_get_path(mt_p mt, uint32_t idx, hash_p root, hash_p *path);
 
-void mt_flush(mt_ptr mt);
-void mt_flush_to(mt_ptr mt, uint32_t idx);
+void mt_flush(mt_p mt);
+void mt_flush_to(mt_p mt, uint32_t idx);
 
 /** Client-side verification
  * @param k The index of the target hash
  * @param j The maximum index + 1 of the tree when the path is generated
  */
-bool mt_verify(uint32_t k, uint32_t j, hash_t *path, signed_hash root);
+bool mt_verify(uint32_t k, uint32_t j, hash_p *path, hash_p root);
