0af59ffc3766dda7617cb200046dca481b142c1f
Chris Hawblitzel <Chris.Hawblitzel@microsoft.com>
2018-11-04 08:16:42 -0800

    In SConstruct, let extract_assembly_code take multiple algorithm files

--- vale/code/crypto/ecc/curve25519/Main25519.ml
+++ vale/code/crypto/ecc/curve25519/Main25519.ml
@@ -2,7 +2,9 @@ let _ =
   CmdLineParser.parse_cmdline [
       ("mul2", (fun win -> X64_FastMul.va_code_fast_mul_stdcall win));
       ("sqr",  (fun win -> X64_FastSqr.va_code_fast_sqr_stdcall win));
+(*
       ("mul1", (fun win -> X64_FastMul.va_code_fast_mul1_stdcall win));
       ("add1", (fun win -> X64_FastMul.va_code_fast_add1_stdcall win));
       ("add",  (fun win -> X64_FastMul.va_code_fast_add_stdcall win));
+*)
     ]
