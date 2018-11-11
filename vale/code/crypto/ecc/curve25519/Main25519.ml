ec4c7d0c4080a361a8c563fade56a664698bf381
Bryan Parno <parno@cmu.edu>
2018-11-10 21:32:14 -0500

    Fix SConstruct so that C test file for ECC code will compile and run

--- vale/code/crypto/ecc/curve25519/Main25519.ml
+++ vale/code/crypto/ecc/curve25519/Main25519.ml
@@ -1,6 +1,6 @@
 let _ =
   CmdLineParser.parse_cmdline [
-      ("mul2", (fun win -> X64_FastMul.va_code_fast_mul_stdcall win));
+      ("mul", (fun win -> X64_FastMul.va_code_fast_mul_stdcall win));
       ("sqr",  (fun win -> X64_FastSqr.va_code_fast_sqr_stdcall win));
       ("mul1", (fun win -> X64_FastUtil.va_code_fast_mul1_stdcall win));
       ("add1", (fun win -> X64_FastUtil.va_code_fast_add1_stdcall win));
