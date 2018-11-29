be12bc00a04b665d027239e4a168f890796fa194
Bryan Parno <parno@cmu.edu>
2018-11-29 15:30:22 -0500

    Reallocate registers to try to minimize push/pops needed to preserve callee-save registers

--- vale/code/crypto/ecc/curve25519/Main25519.ml
+++ vale/code/crypto/ecc/curve25519/Main25519.ml
@@ -7,7 +7,7 @@ let _ =
       ("add",  (fun win -> X64_FastUtil.va_code_fast_add_stdcall win));
       ("sub1", (fun win -> X64_FastUtil.va_code_fast_sub1_stdcall win));
       ("sub",  (fun win -> X64_FastUtil.va_code_fast_sub_stdcall win));
-      ("mul1_add", (fun win -> X64_FastHybrid.va_code_fast_mul1_add_stdcall win));
+      (* ("mul1_add", (fun win -> X64_FastHybrid.va_code_fast_mul1_add_stdcall win)); *)
       ("fadd", (fun win -> X64_FastHybrid.va_code_fadd_stdcall win));
       ("fsub", (fun win -> X64_FastHybrid.va_code_fsub_stdcall win));
       ("fmul1", (fun win -> X64_FastHybrid.va_code_fmul1_stdcall win));
