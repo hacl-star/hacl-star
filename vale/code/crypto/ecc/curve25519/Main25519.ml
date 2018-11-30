4174185a5eddc4cd47855a0eb578caa12ac8ef09
Bryan Parno <parno@cmu.edu>
2018-11-29 22:26:39 -0500

    Fix extraction

--- vale/code/crypto/ecc/curve25519/Main25519.ml
+++ vale/code/crypto/ecc/curve25519/Main25519.ml
@@ -1,7 +1,9 @@
 let _ =
   CmdLineParser.parse_cmdline [
-      ("mul", (fun win -> X64_FastMul.va_code_fast_mul_stdcall win));
+      ("mul",  (fun win -> X64_FastMul.va_code_fast_mul_stdcall win));
+      ("mul2", (fun win -> X64_FastMul.va_code_fast_mul2_stdcall win));
       ("sqr",  (fun win -> X64_FastSqr.va_code_fast_sqr_stdcall win));
+      ("sqr2", (fun win -> X64_FastSqr.va_code_sqr2_stdcall win));
       ("mul1", (fun win -> X64_FastUtil.va_code_fast_mul1_stdcall win));
       ("add1", (fun win -> X64_FastUtil.va_code_fast_add1_stdcall win));
       ("add",  (fun win -> X64_FastUtil.va_code_fast_add_stdcall win));
