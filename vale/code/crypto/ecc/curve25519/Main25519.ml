f3a9b012e7bf87763ec5bb02ccbf5ed1d78aeca9
Bryan Parno <parno@cmu.edu>
2018-12-01 22:49:05 -0500

    Add new functions to the list for extraction

--- vale/code/crypto/ecc/curve25519/Main25519.ml
+++ vale/code/crypto/ecc/curve25519/Main25519.ml
@@ -14,4 +14,8 @@ let _ =
       ("fsub", (fun win -> X64_FastHybrid.va_code_fsub_stdcall win));
       ("fmul1", (fun win -> X64_FastHybrid.va_code_fmul1_stdcall win));
       ("carry_wide", (fun win -> X64_FastHybrid.va_code_carry_wide_stdcall win));
+      ("fmul",  (fun win -> X64_FastWide.va_code_fmul_stdcall win));
+      ("fmul2", (fun win -> X64_FastWide.va_code_fmul2_stdcall win));
+      ("fsqr",  (fun win -> X64_FastWide.va_code_fsqr_stdcall win));
+      ("fsqr2", (fun win -> X64_FastWide.va_code_fsqr2_stdcall win));
     ]
