d31a2c600ebf86e9024c86b4cd6cd85dad60f4cd
Bryan Parno <parno@cmu.edu>
2019-02-06 23:06:21 -0500

    Start traversing the AST

--- vale/scripts/ingest-openssl-perl.py
+++ vale/scripts/ingest-openssl-perl.py
@@ -7,6 +7,7 @@ import sys
 
 # For details, see: https://github.com/lark-parser/lark
 from lark import Lark, Transformer, UnexpectedToken, UnexpectedCharacters, v_args  # pip install lark-parser
+import lark
 
 sys.setrecursionlimit(10000)
 
@@ -27,12 +28,16 @@ grammar = """
     label_target: "." NAME
     op: const | base | mem_op | label_target
     ops: [op ("," op)*]
-    instruction: NAME ops COMMENT?
+    comment: COMMENT
+    instruction: NAME ops comment?
     label: "." NAME ":"
 
     ignored: ALIGN | TYPE | SIZE
-    proc_decl: NAME ":"    
-    procedure: proc_decl (instruction|label|COMMENT|ignored)+
+    proc_decl: NAME ":"   
+    _proc_item: instruction
+              | label
+              | ignored
+    procedure: proc_decl _proc_item+
     code: (COMMENT|ignored)* ".text" (COMMENT|ignored)* procedure+ (COMMENT|ignored)* 
 
     ALIGN: ".align" /.*/
@@ -55,30 +60,33 @@ _aesni_ctr32_ghash_6x:
     	vmovdqu		0x20($const),$T2	# borrow $T2, .Lone_msb
 	sub		\$6,$len
 	vpxor		$Z0,$Z0,$Z0		# $Z0   = 0
-        vmovdqu		0x00-0x80($key),$rndkey
-	vpaddb		$T2,$T1,$inout1
-        vmovdqu		$Z0,16+8(%rsp)		# "$Z3" = 0
-	jmp		.Loop6x
 
 .Loop6x:
-	add		\$`6<<24`,$counter
-	jc		.Lhandle_ctr32		# discard $inout[1-5]?
-	vmovdqu		0x00-0x20($Xip),$Hkey	# $Hkey^1
-	  vpaddb	$T2,$inout5,$T1		# next counter value
-	  vpxor		$rndkey,$inout1,$inout1
-	  vpxor		$rndkey,$inout2,$inout2
-
-.text
-
 """
 
-def print_proc(procname):
+#        vmovdqu		0x00-0x80($key),$rndkey
+#	vpaddb		$T2,$T1,$inout1
+#        vmovdqu		$Z0,16+8(%rsp)		# "$Z3" = 0
+#	jmp		.Loop6x
+#
+#.Loop6x:
+#	add		\$`6<<24`,$counter
+#	jc		.Lhandle_ctr32		# discard $inout[1-5]?
+#	vmovdqu		0x00-0x20($Xip),$Hkey	# $Hkey^1
+#	  vpaddb	$T2,$inout5,$T1		# next counter value
+#	  vpxor		$rndkey,$inout1,$inout1
+#	  vpxor		$rndkey,$inout2,$inout2
+#
+#"""
+
+def print_proc_header(procname):
     print "procedure {:quick} %s ()" % procname
     print "\t\tlets"
     print "\t\t\tinp @= rdi; out @= rsi; len @= rdx; key @= rcx; ivp @= r8; Xip @= r9;"
     print "\t\t\tIi @= xmm0; T1 @= xmm1; T2 @= xmm2; Hkey @= xmm3; Z0 @= xmm4;"
     print "\t\t\tZ1 @= xmm5; Z2 @= xmm6; Z3 @= xmm7; Xi @= xmm8;"
-    print "\t\t\tinout0 @= xmm9; inout1 @= xmm10; inout2 @= xmm11; inout3 @= xmm12; inout4 @= xmm13; inout5 @= xmm14; rndkey @= xmm15;"
+    print "\t\t\tinout0 @= xmm9; inout1 @= xmm10; inout2 @= xmm11; inout3 @= xmm12;"
+    print "\t\t\tinout4 @= xmm13; inout5 @= xmm14; rndkey @= xmm15;"
     print "\t\t\tcounter @= ebx; rounds @= ebp; ret @= r10; const @= r11; in0 @= r14; end0 @= r15;"
     print "\n\t\treads"
     print "\t\t\tmemTaint;"
@@ -94,7 +102,8 @@ def parse(filename):
     ast = None
     with open(filename) as f:
         try:
-            ast = parser.parse(f.read())
+            #ast = parser.parse(f.read())
+            ast = parser.parse(example)
         except UnexpectedToken as e:
             print e
             print e.considered_rules
@@ -107,8 +116,36 @@ def test():
     parser = Lark(grammar)
     print(parser.parse(example).pretty())
 
+
+def print_instr(nodes):
+    print "Intr name: %s" % nodes[0]
+    ops = nodes[1]
+    if len(nodes) > 2:
+        print "Comment: %s" % nodes[2].children[0]
+
+def print_procedure(nodes):
+    for n in nodes:
+#        print "Checking %s" % n
+#        print "It's of type %s " % type(n)
+        if n.data == 'proc_decl':
+            print "Found decl: %s" % n.children[0]
+            #print_proc_header(n.children[0])
+        elif n.data == 'label':
+            print "\t// Label: .%s" % n.children[0]
+        elif n.data == 'instruction':
+            print_instr(n.children)
+        else:
+            print "WARNING: Unknown node: %s" % n
+
 def print_vale(ast):
-    print ast
+    print ast.pretty()
+    #print ast.children[0]
+    #print ast.children[0].children[0]
+    for child in ast.children[0].children:  # Skip to the contents of `code`
+        if child.data == 'ignored':
+            continue
+        if child.data == 'procedure':
+            print_procedure(child.children)
 
 
 ############  Argument parsing and dispatch  ##################
