11e262fcd9e48de618a8a5846fa5c0913e1054c4
Mikhail Volkhov <volhovm.cs@gmail.com>
2019-08-10 15:59:54 +0200

    Permute Pail/GM slots w/o protocol

--- code/experimental/he/hestarhs/src/Argmax.hs
+++ code/experimental/he/hestarhs/src/Argmax.hs
@@ -128,69 +128,6 @@ argmaxServer sock skTop skDGK skGM l m r = do
 ---- Log argmax
 --------------------------------------------------------------------------------
 
--- Permutes list of 2k elements (passed as 2 ciphertexts) into 2
--- another ciphertexts.
-permuteFoldClient ::
-       Pahe sTop
-    => Socket Req
-    -> PahePk sTop
-    -> Int
-    -> [Int]
-    -> PaheCiph sTop
-    -> PaheCiph sTop
-    -> IO (PaheCiph sTop, PaheCiph sTop)
-permuteFoldClient sock pk l perm v1 v2 = do
-    let k = paheK pk
-    let k2 = k `div` 2
-    when (k2 * 2 /= k) $ error "permuteFoldClient doesn't support non-even k"
-
-    let perminv = map (\j ->fromMaybe (error "permuteClient perminv") $
-                        findIndex (==j) perm) [0..2*k-1]
-
-    mask1Raw <- replicateM k $ randomRIO (0, 2 ^ l - 1)
-    mask2Raw <- replicateM k $ randomRIO (0, 2 ^ l - 1)
-    mask1Enc <- paheEnc pk mask1Raw
-    mask2Enc <- paheEnc pk mask2Raw
-    v1Masked <- paheSIMDAdd pk v1 mask1Enc
-    v2Masked <- paheSIMDAdd pk v2 mask2Enc
-
-    sendMulti sock =<< NE.fromList <$> mapM (paheToBS pk) [v1Masked,v2Masked]
-
-    -- k ciphertexts containing valuesMasked_i in all slots
-    replElems <- mapM (paheFromBS pk) =<< receiveMulti sock
-
-    let loop w j = if j == k then pure (paheZero pk) else do
-            next <- loop w (j+1)
-            let i = perminv !! (w+j)
-            -- We select jth element from (perm^-1 j)
-            let selMask = replicate j 0 ++ [1] ++ replicate (k - j - 1) 0
-            cur <- paheSIMDMulScal pk (replElems !! i) selMask
-            paheSIMDAdd pk cur next
-
-    permuted1 <- loop 0 0
-    permuted2 <- loop k 0
-
-    let permutedMaskNeg = map (\i -> negate ((mask1Raw++mask2Raw) !! (perminv !! i))) [0..2*k-1]
-    let m1 = take k permutedMaskNeg
-    let m2 = drop k permutedMaskNeg
-
-    r1 <- paheSIMDAdd pk permuted1 =<< paheEnc pk m1
-    r2 <- paheSIMDAdd pk permuted2 =<< paheEnc pk m2
-
-    pure (r1,r2)
-
-permuteFoldServer ::
-       Pahe sTop
-    => Socket Rep
-    -> PaheSk sTop
-    -> IO ()
-permuteFoldServer sock sk = do
-    let pk = paheToPublic sk
-    [p1,p2] <- mapM (paheDec sk <=< paheFromBS pk) =<< receiveMulti sock
-    let dups = map (\e -> replicate (paheK pk) e) $ p1 ++ p2
-    encDups <- mapM (paheEnc pk) dups
-    sendMulti sock =<< (NE.fromList <$> mapM (paheToBS pk) encDups)
-
 
 -- TODO we're missing the full power of the system because the first
 -- comparison uses 2^(m-1) slots only.
@@ -213,7 +150,8 @@ argmaxLog2Client sock pkTop pkDGK pkGM l m v1 v2 = do
     perm <- shuffle [0..2*k-1]
     log $ "Client: perm " <> show perm
     let perminv j = fromMaybe (error "argmaxLogCli perminv") $ findIndex (==j) perm
-    (v1Perm,v2Perm) <- measureTimeSingle "perm" $ permuteFoldClient sock pkTop l perm v1 v2
+    (v1Perm,v2Perm) <-
+        measureTimeSingle "perm" $ pahePermute pkTop v1 v2 perm
     log "Client: perm done"
 
     -- p1 and p2 are of length 2^i each
@@ -265,7 +203,7 @@ argmaxLog2Client sock pkTop pkDGK pkGM l m v1 v2 = do
 
     maxIndex <- fromIntegral . w64FromBs <$> receive sock
 
-    pure $ perminv maxIndex
+    pure $ perm !! maxIndex
 
 argmaxLog2Server ::
        (Pahe sTop, Pahe sDGK, Pahe sGM)
@@ -280,7 +218,7 @@ argmaxLog2Server sock skTop skDGK skGM l m = do
     let pkTop = paheToPublic skTop
     let pkGM = paheToPublic skGM
 
-    permuteFoldServer sock skTop
+    pahePermuteServ skTop
 
     -- at step i we're comparing two arrays of size 2^i each
     -- so the first step has index m-1
@@ -344,13 +282,13 @@ _testArgmax =
 
       putTextLn "Keygen..."
       -- SIMD parameter
-      let k = 16
+      let k = 1
       -- Parallelism level used in linear argmax, should be less than k
       let r = 1
       -- bit size of numbers we compare
       let l = 64
       -- Number of argmax input elements
-      let m::Int = 2 ^ (log2 (fromIntegral k) - 1 :: Integer)
+      let m::Int = 32 -- 2 ^ (log2 (fromIntegral k) - 1 :: Integer)
       --let m::Int = 20
       let mlog::Int = log2 (m-1)
       -- plaintext space size
@@ -454,7 +392,7 @@ _testArgmax =
               putTextLn "OK"
 
 
-      testLogArgmax
+      --testLogArgmax
       testArgmax
 
       --testPermFold
