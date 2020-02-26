3a08acd8df51d5081d6531b3f29c5784437accb9
Victor Dumitrescu <v.dumitrescu@outlook.com>
2020-02-26 15:06:38 +0100

    Updated Ctypes snapshot after changes in KreMLin

--- dist/gcc-compatible/lib/Hacl_Blake2b_32_bindings.ml
+++ dist/gcc-compatible/lib/Hacl_Blake2b_32_bindings.ml
@@ -5,16 +5,9 @@ module Bindings(F:Cstubs.FOREIGN) =
     let hacl_Blake2b_32_blake2b =
       foreign "Hacl_Blake2b_32_blake2b"
         (uint32_t @->
-           ((ptr uint8_t) @->
-              (uint32_t @->
-                 ((ptr uint8_t) @->
-                    (uint32_t @-> ((ptr uint8_t) @-> (returning void)))))))
-    let hacl_Blake2b_32_blake2b_bytes =
-      foreign "Hacl_Blake2b_32_blake2b"
-        (uint32_t @->
            (ocaml_bytes @->
               (uint32_t @->
                  (ocaml_bytes @->
                     (uint32_t @-> (ocaml_bytes @-> (returning void)))))))
-
+      
   end
