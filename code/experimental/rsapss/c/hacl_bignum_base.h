548dc5c4528721cec9abc7a116b49a1572f4bbc0
Marina Polubelova <polubelovam@gmail.com>
2020-03-18 22:41:30 +0100

    use intrinsics as macros

--- code/experimental/rsapss/c/hacl_bignum_base.h
+++ code/experimental/rsapss/c/hacl_bignum_base.h
@@ -2,7 +2,6 @@
 #include <immintrin.h>
 #include <x86intrin.h>
 
-#include "Hacl_Bignum_Base.h"
 
 /*
 unsigned char _addcarry_u64 (unsigned char c_in, unsigned __int64 a, unsigned __int64 b, unsigned __int64 * out)
@@ -12,9 +11,9 @@ Add unsigned 64-bit integers a and b with unsigned 8-bit carry-in c_in (carry fl
 and store the unsigned 64-bit result in out, and the carry-out in dst (carry or overflow flag).
 */
 
-uint64_t Hacl_Bignum_Base_addcarry_u64_st(uint64_t c_in, uint64_t a, uint64_t b, uint64_t *out) {
-  return _addcarry_u64(c_in, a, b, (long long unsigned int *)out);
-}
+#define Hacl_Bignum_Base_addcarry_u64_st(c_in, a, b, out) \
+  (_addcarry_u64(c_in, a, b, (long long unsigned int *) out))
+
 
 /*
 unsigned char _subborrow_u64 (unsigned char b_in, unsigned __int64 a, unsigned __int64 b, unsigned __int64 * out)
@@ -23,6 +22,7 @@ Description
 Add unsigned 8-bit borrow b_in (carry flag) to unsigned 64-bit integer b, and subtract the result from
 unsigned 64-bit integer a. Store the unsigned 64-bit result in out, and the carry-out in dst (carry or overflow flag).
 */
-uint64_t Hacl_Bignum_Base_subborrow_u64_st(uint64_t b_in, uint64_t a, uint64_t b, uint64_t *out) {
-  return _subborrow_u64(b_in, a, b, (long long unsigned int *)out);
-}
+
+#define Hacl_Bignum_Base_subborrow_u64_st(b_in, a, b, out)  \
+  (_subborrow_u64(b_in, a, b, (long long unsigned int *) out))
+
