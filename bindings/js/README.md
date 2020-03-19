374728d4b8517f67278895c0bcd1289a8d2e44c4
Denis Merigoux <denis.merigoux@gmail.com>
2020-03-19 12:26:47 +0100

    Moved doc to separate file

---
+++ bindings/js/README.md
@@ -0,0 +1,12 @@
+# HACL* Javascript bindings
+
+## Documentation
+
+Run the file `api_doc.js`. This will create a new file `doc/readable_api.js`.
+To produce the JSDoc documentation of the HACL Javascript API, please run
+
+```
+jsdoc doc/readable_api.js -d doc/out
+```
+
+You can browse the documentation by opening `doc/out/index.html` in your web browser.
---
+++ bindings/js/api_doc.js
@@ -0,0 +1,72 @@
+var api_json = require("./api.json");
+var fs = require('fs');
+
+
+// Generates a file wherez every function is exanded for doc purposes
+fs.writeFile("doc/readable_api.js", (function() {
+  var contents =
+    "var HaclWasm = require('../api.js');\n\
+/**\n\
+ * Hacl bindings for Javascript using WebAssembly.\n\
+ * @module Hacl\n\
+ */";
+  Object.keys(api_json).map(function(key_module) {
+    contents += "\n\n"
+    contents += "/**\n"
+    contents += " * @namespace " + key_module + "\n"
+    contents += " */\n"
+    contents += "var  " + key_module + " = {\n"
+    Object.keys(api_json[key_module]).map(function(key_func) {
+      contents += "\n\n"
+      contents += "/**\n"
+      // Params info
+      api_json[key_module][key_func].args.filter((arg) =>
+        arg.interface_index !== undefined
+      ).sort(function(a1, a2) {
+        a1.interface_index - a2.interface_index
+      }).map((arg) => {
+        contents += " * @param {" + arg.type + "} " + arg.name
+        if (arg.type === "buffer" && typeof arg.size === "number") {
+          contents += " - size " + arg.size
+        }
+        if (arg.type === "buffer" && typeof arg.size === "string") {
+          contents += " - size " + arg.size
+        }
+        contents += "\n"
+      });
+      // Return info
+      if (api_json[key_module][key_func].return.type !== "void") {
+        contents += " * @return {" + api_json[key_module][key_func].return.type + "}\n"
+      }
+      api_json[key_module][key_func].args.filter((arg) =>
+        arg.kind === "output"
+      ).map((arg) => {
+        contents += " * @return {" + arg.type + "} " + arg.name
+        if (arg.type === "buffer" && typeof arg.size === "number") {
+          contents += " - size " + arg.size
+        }
+        if (arg.type === "buffer" && typeof arg.size === "string") {
+          contents += " - size " + arg.size
+        }
+        contents += "\n"
+      });
+      contents += " */\n"
+      contents += key_func + ": function("
+      api_json[key_module][key_func].args.filter((arg) =>
+        arg.interface_index !== undefined
+      ).sort(function(a1, a2) {
+        a1.interface_index - a2.interface_index
+      }).map((arg) => {
+        contents += arg.name + ","
+      });
+      contents += ") { " + key_func + ": HaclWasm." + key_module + "." + key_func + ".apply(null, arguments) },\n"
+    })
+    contents += "}\n"
+    contents += "module.exports." + key_module + " = " + key_module + "\n"
+  })
+  return contents
+})(), 'utf8', function(err, data) {
+  if (err !== null) {
+    console.log(err)
+  }
+})
