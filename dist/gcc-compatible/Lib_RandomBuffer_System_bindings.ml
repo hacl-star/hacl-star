9af01f18d6676e7bdc11dd20bc7a1f8794b4842f
Victor Dumitrescu <v.dumitrescu@outlook.com>
2020-04-17 17:35:21 +0200

    A few more nits + update opam archive

--- dist/gcc-compatible/Lib_RandomBuffer_System_bindings.ml
+++
@@ -1,8 +0,0 @@
-open Ctypes
-module Bindings(F:Cstubs.FOREIGN) =
-struct
-  open F
-  let randombytes =
-    foreign "Lib_RandomBuffer_System_randombytes"
-      (ocaml_bytes @-> uint32_t @-> returning bool)
-end
