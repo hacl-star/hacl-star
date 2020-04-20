0914dc0347e4f3c7473b91dfc5d98555309ddd54
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2020-04-20 13:18:15 -0700

    One more entry in the test matrix

--- .ci/script.sh
+++ .ci/script.sh
@@ -8,21 +8,32 @@ set -o pipefail
 # other configuration.
 for p in /usr/local/Cellar/openssl@1.1/*; do export CFLAGS="-I$p/include/"; export LDFLAGS="-L$p/lib"; done
 
-# Running without OCaml -- need to configure
-cd dist/gcc-compatible && ./configure
-
+# Most likely running without OCaml -- need to configure
+pushd dist/gcc-compatible
+./configure
 make -j
+popd
 
 if [[ "$(uname -m | cut -c 1-7)" == "aarch64" ]]; then
-  make -C ../../tests arm -j
+  make -C tests arm -j
 else
-  make -C ../../tests -j test
+  make -C tests -j test
 
   # Extracted C tests -- need full kremlib, don't work on ARM because of
   # intrinsics for x86 in cpu cycle count routines in testlib.c
-  cd ../test/c/
+  pushd dist/test/c/
   git clone https://github.com/fstarlang/kremlin -b protz_ci --depth 10
   export KREMLIN_HOME=$(pwd)/kremlin
   make -C kremlin/kremlib/dist/generic -f Makefile.basic -j
   make -j -k
+  popd
+fi
+
+if which opam; then
+  make -C dist/gcc-compatible install-hacl-star-raw
+  pushd bindings/ocaml
+  dune build
+  dune runtest
+  dune install
+  popd
 fi
