631b24b003e1b0c0b76b9d2386c39777777a9c2b
Ann Weine <11855463+Frosne@users.noreply.github.com>
2020-04-28 13:56:25 +0200

    wip

--- tests/ecdhp256-test.c
+++ tests/ecdhp256-test.c
@@ -24,16 +24,31 @@ int main()
 
 	for (int i = 0 ; i< sizeof(i_vectors)/sizeof(ecdhp256_tv_i); i++)
 	{
-
+		printf("ECDH Initiator Test %d \n", i );
 		uint64_t success = Hacl_Impl_P256_DH_ecp256dh_i(result, i_vectors[i].privateKey);
 		ok = ok && (success == 0);
 		ok = ok && compare_and_print(32, result, i_vectors[i].expectedPublicKeyX);
 		ok = ok && compare_and_print(32, result + 32, i_vectors[i].expectedPublicKeyY);
 	}
 
+	
+	uint8_t* pk = (uint8_t*) malloc (sizeof (uint8_t) * 64);
+	
+	for (int i = 0 ; i< sizeof(i_vectors)/sizeof(ecdhp256_tv_i); i++)
+	{
 
-	printf("%lu\n", ok);
+		printf("ECDH Responder Test %d\n", i );
+		memcpy(pk, i_vectors[0].publicKeyX1,  32);
+		memcpy(pk+32, i_vectors[0].publicKeyY1,  32);
+	   
+	    uint64_t success = Hacl_Impl_P256_DH_ecp256dh_r(result, pk, i_vectors[0].privateKey);
+	    ok = ok && (success == 0);
+	    ok = ok && compare_and_print(32, result, i_vectors[0].expectedResult);
+	}
+
+
   	if (ok) return EXIT_SUCCESS;
   	else return EXIT_FAILURE;
+
 
 }
