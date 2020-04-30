2e14692eb926c0987fd5d4e962c8cc9027bc8960
Marina Polubelova <polubelovam@gmail.com>
2020-05-01 01:37:36 +0200

    add comparison with sha512-openssl

--- tests/sha2-vec-test.c
+++ tests/sha2-vec-test.c
@@ -9,6 +9,7 @@
 #include "Hacl_SHA2_Scalar32.h"
 #include "Hacl_SHA2_Vec128.h"
 #include "Hacl_SHA2_Vec256.h"
+//#include "Hacl_SHA2_Vec512.h"
 
 #include "sha2_vectors.h"
 #include "sha2vec_vectors.h"
@@ -26,6 +27,13 @@ void ossl_sha2(uint8_t* hash, uint8_t* input, int len){
    //EVP_EncryptUpdate(ctx, cipher, &clen, plain, len);
    //EVP_EncryptFinal_ex(ctx, cipher + clen, &clen);
    //EVP_CIPHER_CTX_free(ctx);
+}
+
+void ossl_sha512(uint8_t* hash, uint8_t* input, int len){
+  SHA512_CTX ctx;
+  SHA512_Init(&ctx);
+  SHA512_Update(&ctx,input,len);
+  SHA512_Final(hash,&ctx);
 }
 
 #define ROUNDS 16384
@@ -51,6 +59,10 @@ bool print_test1(uint8_t* in, int in_len, uint8_t* exp256, uint8_t* exp512){
   ossl_sha2(comp256,in,in_len);
   printf("OpenSSL SHA2-256 (32-bit) Result:\n");
   ok = print_result(comp256,exp256,32) && ok;
+
+  ossl_sha512(comp512,in,in_len);
+  printf("OpenSSL SHA2-512 (32-bit) Result:\n");
+  ok = print_result(comp512,exp512,64) && ok;
 
   return ok;
 }
@@ -113,6 +125,32 @@ bool print_test8(uint8_t* in, uint8_t* in1, uint8_t* in2, uint8_t* in3, uint8_t*
   return ok;
 }
 
+/*
+bool print_test8_512(uint8_t* in, uint8_t* in1, uint8_t* in2, uint8_t* in3, uint8_t* in4, uint8_t* in5, uint8_t* in6, uint8_t* in7, int in_len,
+		     uint8_t* exp, uint8_t* exp1, uint8_t* exp2, uint8_t* exp3, uint8_t* exp4, uint8_t* exp5, uint8_t* exp6, uint8_t* exp7){
+  uint8_t comp[64] = {0};
+  uint8_t comp1[64] = {0};
+  uint8_t comp2[64] = {0};
+  uint8_t comp3[64] = {0};
+  uint8_t comp4[64] = {0};
+  uint8_t comp5[64] = {0};
+  uint8_t comp6[64] = {0};
+  uint8_t comp7[64] = {0};
+
+  Hacl_SHA2_Vec512_sha512_8(comp,comp1,comp2,comp3,comp4,comp5,comp6,comp7,in_len,in,in1,in2,in3,in4,in5,in6,in7);
+  printf("VEC8 SHA2-512 (32-bit) Result:\n");
+  bool ok = print_result(comp, exp,64);
+  ok = print_result(comp1,exp1,64) && ok;
+  ok = print_result(comp2,exp2,64) && ok;
+  ok = print_result(comp3,exp3,64) && ok;
+  ok = print_result(comp4,exp4,64) && ok;
+  ok = print_result(comp5,exp5,64) && ok;
+  ok = print_result(comp6,exp6,64) && ok;
+  ok = print_result(comp7,exp7,64) && ok;
+
+  return ok;
+}
+*/
 
 int main()
 {
@@ -132,6 +170,12 @@ int main()
 
   ok &= print_test4_512(vectors_vec[0].input,vectors_vec[1].input,vectors_vec[2].input,vectors_vec[3].input,vectors_vec[0].input_len,
 			vectors_vec[0].tag_512,vectors_vec[1].tag_512,vectors_vec[2].tag_512,vectors_vec[3].tag_512);
+  /*
+  ok &= print_test8_512(vectors_vec[0].input,vectors_vec[1].input,vectors_vec[2].input,vectors_vec[3].input,
+		    vectors_vec[4].input,vectors_vec[5].input,vectors_vec[6].input,vectors_vec[7].input, vectors_vec[0].input_len,
+		    vectors_vec[0].tag_512,vectors_vec[1].tag_512,vectors_vec[2].tag_512,vectors_vec[3].tag_512,
+		    vectors_vec[4].tag_512,vectors_vec[5].tag_512,vectors_vec[6].tag_512,vectors_vec[7].tag_512);
+  */
 
   uint64_t len = SIZE;
   uint8_t plain[SIZE];
@@ -198,12 +242,77 @@ int main()
   double tdiff2a = (double)(t2 - t1);
 
 
+  for (int j = 0; j < ROUNDS; j++) {
+    Hacl_SHA2_Scalar32_sha512(plain,SIZE,plain);
+  }
+
+  t1 = clock();
+  a = cpucycles_begin();
+  for (int j = 0; j < ROUNDS; j++) {
+    Hacl_SHA2_Scalar32_sha512(plain,SIZE,plain);
+  }
+  b = cpucycles_end();
+  t2 = clock();
+  double cdiff1 = b - a;
+  double tdiff1 = (double)(t2 - t1);
+
+
+  for (int j = 0; j < ROUNDS; j++) {
+    Hacl_SHA2_Vec256_sha512_4(plain,plain+64,plain+128,plain+192,SIZE,plain,plain,plain,plain);
+  }
+
+  t1 = clock();
+  a = cpucycles_begin();
+  for (int j = 0; j < ROUNDS; j++) {
+    Hacl_SHA2_Vec256_sha512_4(plain,plain+64,plain+128,plain+192,SIZE,plain,plain,plain,plain);
+  }
+  b = cpucycles_end();
+  t2 = clock();
+  double cdiff2 = b - a;
+  double tdiff2 = (double)(t2 - t1);
+
+  /*
+  for (int j = 0; j < ROUNDS; j++) {
+    Hacl_SHA2_Vec512_sha512_8(plain,plain+64,plain+128,plain+192,plain+256,plain+320,plain+384,plain+448,SIZE,plain,plain,plain,plain,plain,plain,plain,plain);
+  }
+
+  t1 = clock();
+  a = cpucycles_begin();
+  for (int j = 0; j < ROUNDS; j++) {
+    Hacl_SHA2_Vec512_sha512_8(plain,plain+64,plain+128,plain+192,plain+256,plain+320,plain+384,plain+448,SIZE,plain,plain,plain,plain,plain,plain,plain,plain);
+  }
+  b = cpucycles_end();
+  t2 = clock();
+  double cdiff3 = b - a;
+  double tdiff3 = (double)(t2 - t1);
+  */
+
+  for (int j = 0; j < ROUNDS; j++) {
+    ossl_sha512(plain,plain,SIZE);
+  }
+  t1 = clock();
+  a = cpucycles_begin();
+  for (int j = 0; j < ROUNDS; j++) {
+    ossl_sha512(plain,plain,SIZE);
+  }
+  b = cpucycles_end();
+  t2 = clock();
+  double cdiff4 = b - a;
+  double tdiff4 = (double)(t2 - t1);
+
+
   uint8_t res = plain[0];
   uint64_t count = ROUNDS * SIZE;
+  printf ("\n\n");  
   printf("NEW SHA2-256 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff2n,cdiff2n);
   printf("VEC4 SHA2-256 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff2v,cdiff2v);
   printf("VEC8 SHA2-256 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff2v8,cdiff2v8);
   printf("OpenSSL SHA2-256 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff2a,cdiff2a);
+  printf ("\n\n");
+  printf("NEW SHA2-512 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff1,cdiff1);
+  printf("VEC4 SHA2-512 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff2,cdiff2);
+  //printf("VEC8 SHA2-512 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff3,cdiff3);
+  printf("OpenSSL SHA2-512 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff4,cdiff4);
 
   if (ok) return EXIT_SUCCESS;
   else return EXIT_FAILURE;
