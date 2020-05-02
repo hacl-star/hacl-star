a40ade7dcdbf1e8a3a30f07e34fbe3501830d136
Dzomo, the Everest Yak <everbld@microsoft.com>
2020-05-02 08:20:41 +0000

    [CI] regenerate hints and dist

--- dist/gcc-compatible/lib_gen/Hacl_HMAC_gen.ml
+++
@@ -1,9 +0,0 @@
-let _ =
-  (((Format.set_formatter_out_channel (open_out_bin "lib/Hacl_HMAC_stubs.ml");
-     Cstubs.write_ml Format.std_formatter ~prefix:""
-       (module Hacl_HMAC_bindings.Bindings));
-    Format.set_formatter_out_channel (open_out_bin "lib/Hacl_HMAC_c_stubs.c"));
-   Format.printf "#include \"Hacl_HMAC.h\"\n");
-  Cstubs.write_c Format.std_formatter ~prefix:""
-    (module Hacl_HMAC_bindings.Bindings)
-  
--- dist/gcc-compatible/lib_gen/Lib_RandomBuffer_System_gen.ml
+++
@@ -1,9 +0,0 @@
-let _ =
-  Format.set_formatter_out_channel (open_out_bin "lib/Lib_RandomBuffer_System_stubs.ml");
-  Cstubs.write_ml Format.std_formatter ~prefix:""
-    (module Lib_RandomBuffer_System_bindings.Bindings);
-  Format.set_formatter_out_channel (open_out_bin "lib/Lib_RandomBuffer_System_c_stubs.c");
-  Format.printf "#include \"Lib_RandomBuffer_System.h\"\n";
-  Cstubs.write_c Format.std_formatter ~prefix:""
-    (module Lib_RandomBuffer_System_bindings.Bindings)
-
