9a2bf7b0aa187bbcc6ed744818a88603ac43b0d0
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2020-05-11 12:04:55 -0700

    OSX implementation since I can test that one

--- lib/c/Lib_Memzero0.c
+++ lib/c/Lib_Memzero0.c
@@ -1,3 +1,8 @@
+#if __APPLE__ && __MACH__
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <string.h>
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
@@ -15,10 +20,14 @@ void Lib_Memzero0_memzero(void *dst, uint64_t len) {
      allocation-time, possibly via KRML_CHECK_SIZE, to fit in a size_t. */
   size_t len_ = (size_t) len;
 
-  /* Default implementation for platforms with no particular support. */
-  #warning "Your platform does not support any safe implementation of memzero -- consider a pull request!"
-  volatile unsigned char *volatile dst_ = (volatile unsigned char *volatile) dst;
-  size_t i = 0U;
-  while (i < len)
-    dst_[i++] = 0U;
+  #if __APPLE__ && __MACH__
+    memset_s(dst, len_, 0, len_);
+  #else
+    /* Default implementation for platforms with no particular support. */
+    #warning "Your platform does not support any safe implementation of memzero -- consider a pull request!"
+    volatile unsigned char *volatile dst_ = (volatile unsigned char *volatile) dst;
+    size_t i = 0U;
+    while (i < len)
+      dst_[i++] = 0U;
+  #endif
 }
