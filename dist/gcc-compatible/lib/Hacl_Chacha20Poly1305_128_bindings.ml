da3c278740a6aca7020ef546e3b4e7aa0b6a2bac
Dzomo, the Everest Yak <everbld@microsoft.com>
2020-05-16 08:20:17 +0000

    [CI] regenerate hints and dist

--- dist/gcc-compatible/lib/Hacl_Chacha20Poly1305_128_bindings.ml
+++ dist/gcc-compatible/lib/Hacl_Chacha20Poly1305_128_bindings.ml
@@ -11,7 +11,6 @@ module Bindings(F:Cstubs.FOREIGN) =
                     (uint32_t @->
                        (ocaml_bytes @->
                           (ocaml_bytes @-> (ocaml_bytes @-> (returning void)))))))))
-      
     let hacl_Chacha20Poly1305_128_aead_decrypt =
       foreign "Hacl_Chacha20Poly1305_128_aead_decrypt"
         (ocaml_bytes @->
@@ -22,5 +21,4 @@ module Bindings(F:Cstubs.FOREIGN) =
                        (ocaml_bytes @->
                           (ocaml_bytes @->
                              (ocaml_bytes @-> (returning uint32_t)))))))))
-      
   end
