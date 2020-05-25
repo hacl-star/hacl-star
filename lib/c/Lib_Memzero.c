3756de256f918e10f2ec3f64bf5fee481e2d298f
Son HO <hosonmarc@gmail.com>
2020-05-25 09:17:57 +0200

    Remove the clear_words function from Lib_Memzero.c

--- lib/c/Lib_Memzero.c
+++ lib/c/Lib_Memzero.c
@@ -1,11 +1,6 @@
 #include <stdint.h>
 #include "Lib_Memzero0.h"
 
-void clear_words(void* mem, unsigned int nwords)
-{ // Clear 32-bit words from memory. "nwords" indicates the number of words to be zeroed.
-  Lib_Memzero0_memzero(mem, 4*len);
-}
-
 void Lib_Memzero_clear_words_u16(unsigned int nwords, uint16_t *mem)
 {
   Lib_Memzero0_memzero((void*)mem, 2*nwords);
