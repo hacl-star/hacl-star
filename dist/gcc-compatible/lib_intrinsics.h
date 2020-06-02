c39990afba4f903060fe965a854eb7afda6a6c13
Ann Weine <11855463+Frosne@users.noreply.github.com>
2020-06-02 15:30:04 +0200

    changed lib

--- dist/gcc-compatible/lib_intrinsics.h
+++ dist/gcc-compatible/lib_intrinsics.h
@@ -1,11 +1,31 @@
 #pragma once
 
 #include <sys/types.h>
-#include <x86intrin.h>
 
 #if __has_include("config.h")
 #include "config.h"
 #endif
+
+
+if defined(__arm__)
+#include "Hacl_IntTypes_Intrinsics.h"
+
+// __arm__ which is defined for 32bit arm, and 32bit arm only.
+// __aarch64__ which is defined for 64bit arm, and 64bit arm only.
+
+#define Lib_IntTypes_Intrinsics_add_carry_u64(x1, x2, x3, x4) \
+  (Hacl_IntTypes_Intrinsics_add_carry_u64(x1, x2, x3, x4))
+
+#define Lib_IntTypes_Intrinsics_sub_borrow_u64(x1, x2, x3, x4) \
+  (Hacl_IntTypes_Intrinsics_sub_borrow_u64(x1, x2, x3, x4))
+
+#endif 
+
+#else 
+	#include <x86intrin.h>
+#endif 
+
+
 
 #if defined(BROKEN_INTRINSICS)
 
@@ -31,6 +51,7 @@
 */
 #if defined(__GNUC__) && !defined (__clang__) && \
   (__GNUC__ < 7 || (__GNUC__ == 7 && (__GNUC_MINOR__ < 2)))
+
 
 #define Lib_IntTypes_Intrinsics_sub_borrow_u64(x1, x2, x3, x4) \
   (_subborrow_u64(x1, x3, x2, (long long unsigned int *) x4))
