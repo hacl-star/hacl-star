eed8032a01804c728978e3cabad73caafa905ad1
Anne Weine <11855463+Frosne@users.noreply.github.com>
2020-06-25 17:20:37 +0200

    Update script.sh

--- .ci/script.sh
+++ .ci/script.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Delete me
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
+
 set -e
 set -o pipefail
 
