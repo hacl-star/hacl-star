47c785f4b8c92ea22df727787bb0246ba6caea01
Jonathan Protzenko <protz@microsoft.com>
2020-07-07 10:48:06 -0700

    Fix syntax of for loops in batch files

--- .ci/script.bat
+++ .ci/script.bat
@@ -3,9 +3,12 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Too
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
 cd dist/msvc-compatible
 cl *.c /I ../kremlin/include /I . /I ../kremlin/kremlib/dist/minimal /c || goto :error
-for /F %i in ('dir /b *-x86_64-msvc.asm') do (ml64 /c %i || goto :error)
+for /F %%i in ('dir /b *-x86_64-msvc.asm') do (
+  ml64 /c %%i || goto :error
+)
 lib *.obj || goto :error
 echo "SUCCESS"
+exit /b 0
 
 :error
 echo "Failed"
