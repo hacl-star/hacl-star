4e65f14a05c2440af31959c1e3fef0a53b678295
Franziskus Kiefer <franziskuskiefer@gmail.com>
2020-08-26 16:13:06 +0200

    trying to find the toolchain ...

--- .ci/script.sh
+++ .ci/script.sh
@@ -16,7 +16,7 @@ fi
 # ARM cross builds are handled a little different
 if [[  $ARM_CROSS_CI == "aarch64-none-linux-gnu" ]]; then
   pushd dist/gcc-compatible
-  export TOOLCHAIN=$HOME/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
+  export TOOLCHAIN=$PWD/../../../gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
   ./configure -target aarch64-none-linux-gnu
   make -j
   popd
