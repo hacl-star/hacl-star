e49914b30264632dac87dd24357c4dd4f2e66b90
Marina Polubelova <polubelovam@gmail.com>
2020-09-28 18:52:57 +0200

    refresh dist

--- dist/gcc-compatible/Hacl_EC_Ed25519.h
+++ dist/gcc-compatible/Hacl_EC_Ed25519.h
@@ -22,8 +22,8 @@
  */
 
 
-#ifndef __Hacl_Ed25519_H
-#define __Hacl_Ed25519_H
+#ifndef __Hacl_EC_Ed25519_H
+#define __Hacl_EC_Ed25519_H
 
 #if defined(__cplusplus)
 extern "C" {
@@ -37,37 +37,27 @@ extern "C" {
 #include "kremlin/internal/target.h"
 
 
-#include "Hacl_Kremlib.h"
-#include "Hacl_Hash.h"
-#include "Hacl_Curve25519_51.h"
+#include "Hacl_Ed25519.h"
 
-void Hacl_Bignum25519_fdifference(uint64_t *a, uint64_t *b);
+void Hacl_EC_Ed25519_mk_point_at_inf(uint64_t *p);
 
-void Hacl_Bignum25519_reduce_513(uint64_t *a);
+void Hacl_EC_Ed25519_mk_base_point(uint64_t *p);
 
-void Hacl_Impl_Ed25519_PointAdd_point_add(uint64_t *out, uint64_t *p, uint64_t *q);
+void Hacl_EC_Ed25519_point_negate(uint64_t *p, uint64_t *out);
 
-void Hacl_Impl_Ed25519_Ladder_point_mul(uint64_t *result, uint8_t *scalar, uint64_t *q);
+void Hacl_EC_Ed25519_point_add(uint64_t *p, uint64_t *q, uint64_t *out);
 
-void Hacl_Impl_Ed25519_PointCompress_point_compress(uint8_t *z, uint64_t *p);
+void Hacl_EC_Ed25519_point_mul(uint8_t *scalar, uint64_t *p, uint64_t *out);
 
-bool Hacl_Impl_Ed25519_PointDecompress_point_decompress(uint64_t *out, uint8_t *s);
+bool Hacl_EC_Ed25519_point_eq(uint64_t *p, uint64_t *q);
 
-bool Hacl_Impl_Ed25519_PointEqual_point_equal(uint64_t *p, uint64_t *q);
+void Hacl_EC_Ed25519_point_compress(uint64_t *p, uint8_t *out);
 
-void Hacl_Ed25519_sign(uint8_t *signature, uint8_t *priv, uint32_t len, uint8_t *msg);
-
-bool Hacl_Ed25519_verify(uint8_t *pub, uint32_t len, uint8_t *msg, uint8_t *signature);
-
-void Hacl_Ed25519_secret_to_public(uint8_t *pub, uint8_t *priv);
-
-void Hacl_Ed25519_expand_keys(uint8_t *ks, uint8_t *priv);
-
-void Hacl_Ed25519_sign_expanded(uint8_t *signature, uint8_t *ks, uint32_t len, uint8_t *msg);
+bool Hacl_EC_Ed25519_point_decompress(uint8_t *s, uint64_t *out);
 
 #if defined(__cplusplus)
 }
 #endif
 
-#define __Hacl_Ed25519_H_DEFINED
+#define __Hacl_EC_Ed25519_H_DEFINED
 #endif
