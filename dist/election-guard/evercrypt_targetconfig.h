ff8315d4449babc30397f203dad420799d58817e
Jonathan Protzenko <protz@microsoft.com>
2020-10-06 11:05:54 -0400

    New dist folder

---
+++ dist/election-guard/evercrypt_targetconfig.h
@@ -0,0 +1,27 @@
+#ifndef __EVERCRYPT_TARGETCONFIG_H
+#define __EVERCRYPT_TARGETCONFIG_H
+
+/* References:
+ * - https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros?view=vs-2017
+ * - https://sourceforge.net/p/predef/wiki/Architectures/
+ */
+
+#if defined(__x86_64__) || defined(_M_X64)
+#define EVERCRYPT_TARGETCONFIG_X64 1
+#elif defined(__i386__) || defined(_M_IX86)
+#define EVERCRYPT_TARGETCONFIG_X86 1
+#elif defined(__aarch64__) || defined(_M_ARM64)
+#define EVERCRYPT_TARGETCONFIG_AARCH64 1
+#elif defined(__arm__) || defined(_M_ARM)
+#define EVERCRYPT_TARGETCONFIG_AARCH32 1
+#endif
+
+#if __has_include("config.h")
+#include "config.h"
+#endif
+
+#if defined(__GNUC__) && !defined(BROKEN_INLINE_ASM)
+#define EVERCRYPT_TARGETCONFIG_GCC 1
+#endif
+
+#endif
