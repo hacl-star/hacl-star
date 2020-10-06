b7ff1e4231886d1170847219b836b296adee7318
Jonathan Protzenko <protz@microsoft.com>
2020-10-06 11:06:07 -0400

    Refresh dist

--- dist/mitls/Hacl_Bignum256.h
+++ dist/mitls/Hacl_Bignum256.h
@@ -41,6 +41,23 @@ extern "C" {
 #include "Hacl_Kremlib.h"
 #include "Hacl_Bignum.h"
 
+/*******************************************************************************
+
+A verified 256-bit bignum library.
+
+This is a 64-bit optimized version, where bignums are represented as an array
+of four unsigned 64-bit integers, i.e. uint64_t[4]. Furthermore, the
+limbs are stored in little-endian format, i.e. the least significant limb is at
+index 0. Each limb is stored in native format in memory. Example:
+
+  uint64_t sixteen[4] = { 0x10; 0x00; 0x00; 0x00 }
+
+We strongly encourage users to go through the conversion functions, e.g.
+bn_from_bytes_be, to i) not depend on internal representation choices and ii)
+have the ability to switch easily to a 32-bit optimized version in the future.
+
+*******************************************************************************/
+
 /************************/
 /* Arithmetic functions */
 /************************/
