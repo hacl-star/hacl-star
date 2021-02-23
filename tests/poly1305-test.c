b18064ed6264d00e86411d9c92623bf247eae4d3
Son Ho <hosonmarc@gmail.com>
2021-02-23 16:00:23 +0100

    Update poly1305-test.c

--- tests/poly1305-test.c
+++ tests/poly1305-test.c
@@ -9,12 +9,15 @@
 #include <stdbool.h>
 #include <time.h>
 
+#include "test_helpers.h"
+
 #include "Hacl_Poly1305_32.h"
 #include "Hacl_Poly1305_128.h"
+#if defined(TEST_VEC256)
 #include "Hacl_Poly1305_256.h"
+#endif
 #include "EverCrypt_AutoConfig2.h"
 
-#include "test_helpers.h"
 #include "poly1305_vectors.h"
 
 #define ROUNDS 100000
@@ -35,11 +38,13 @@ bool print_test(int in_len, uint8_t* in, uint8_t* key, uint8_t* exp){
   printf("Poly1305 (128-bit) Result:\n");
   ok = ok && print_result(comp, exp);
 
+#if defined(TEST_VEC256)
   if (EverCrypt_AutoConfig2_has_avx2()) {
     Hacl_Poly1305_256_poly1305_mac(comp,in_len,in,key);
     printf("Poly1305 (256-bit) Result:\n");
     ok = ok && print_result(comp, exp);
   }
+#endif
   return ok;
 }
 
@@ -93,7 +98,7 @@ int main() {
   clock_t tdiff2 = t2 - t1;
   cycles cdiff2 = b - a;
 
-
+#if defined(TEST_VEC256)
   if (EverCrypt_AutoConfig2_has_avx2()) {
     memset(plain,'P',SIZE);
     memset(key,'K',16);
@@ -112,13 +117,17 @@ int main() {
   }
   clock_t tdiff3 = t2 - t1;
   cycles cdiff3 = b - a;
+#endif
 
   uint64_t count = ROUNDS * SIZE;
   printf("Poly1305 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff1,cdiff1);
   printf("Poly1305 (128-bit) PERF:\n"); print_time(count,tdiff2,cdiff2);
+
+#if defined(TEST_VEC256)
   if (EverCrypt_AutoConfig2_has_avx2()) {
     printf("Poly1305 (256-bit) PERF:\n"); print_time(count,tdiff3,cdiff3);
   }
+#endif
 
   if (ok) return EXIT_SUCCESS;
   else return EXIT_FAILURE;
