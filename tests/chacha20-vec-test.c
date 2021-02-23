c961d7dc1e009bdc18539dd3ede24196bd8a24e9
Son Ho <hosonmarc@gmail.com>
2021-02-23 15:54:58 +0100

    Update chaca20-vec-test.c

--- tests/chacha20-vec-test.c
+++ tests/chacha20-vec-test.c
@@ -9,12 +9,15 @@
 #include <time.h>
 #include <stdbool.h>
 
+#include "test_helpers.h"
+
 #include "Hacl_Chacha20_Vec32.h"
 #include "Hacl_Chacha20_Vec128.h"
+#if defined(TEST_VEC256)
 #include "Hacl_Chacha20_Vec256.h"
+#endif
 #include "EverCrypt_AutoConfig2.h"
 
-#include "test_helpers.h"
 #include "chacha20_vectors.h"
 
 #define ROUNDS 100000
@@ -37,11 +40,13 @@ bool print_test(int in_len, uint8_t* in, uint8_t* key, uint8_t* nonce, uint8_t* 
   printf("Chacha20 (128-bit) Result:\n");
   ok = ok && print_result(in_len,comp,exp);
 
+#if defined(TEST_VEC256)
   if (EverCrypt_AutoConfig2_has_avx2()) {
     Hacl_Chacha20_Vec256_chacha20_encrypt_256(in_len,comp,in,key,nonce,1);
     printf("Chacha20 (256-bit) Result:\n");
     ok = ok && print_result(in_len,comp,exp);
   }
+#endif
   return ok;
 }
 
@@ -102,6 +107,7 @@ int main() {
   memset(key,'K',16);
   memset(nonce,'N',12);
 
+#if defined(TEST_VEC256)
   if (EverCrypt_AutoConfig2_has_avx2()) {
     for (int j = 0; j < ROUNDS; j++) {
       Hacl_Chacha20_Vec256_chacha20_encrypt_256(SIZE,plain,plain,key,nonce,1);
@@ -117,14 +123,16 @@ int main() {
   }
   double diff3 = t2 - t1;
   uint64_t cyc3 = b - a;
+#endif
 
   uint64_t count = ROUNDS * SIZE;
   printf("32-bit Chacha20\n"); print_time(count,diff1,cyc1);
   printf("128-bit Chacha20\n"); print_time(count,diff2,cyc2);
+#if defined(TEST_VEC256)
   if (EverCrypt_AutoConfig2_has_avx2()) {
     printf("256-bit Chacha20\n"); print_time(count,diff3,cyc3);
   }
-
+#endif
   if (ok) return EXIT_SUCCESS;
   else return EXIT_FAILURE;
 }
