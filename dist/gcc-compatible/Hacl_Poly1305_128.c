71ec7cf578475f1d1919f41a7d4152abb89dc171
Son Ho <hosonmarc@gmail.com>
2021-02-24 11:25:03 +0100

    Add more printf in Poly1305

--- dist/gcc-compatible/Hacl_Poly1305_128.c
+++ dist/gcc-compatible/Hacl_Poly1305_128.c
@@ -82,11 +82,19 @@ Hacl_Impl_Poly1305_Field32xN_128_load_acc2(Lib_IntVector_Intrinsics_vec128 *acc,
   Lib_IntVector_Intrinsics_vec128 e[5U];
   for (uint32_t _i = 0U; _i < (uint32_t)5U; ++_i)
     e[_i] = Lib_IntVector_Intrinsics_vec128_zero;
+
+  print_debug_buf8("load_acc2 #b1 src", b);
   Lib_IntVector_Intrinsics_vec128 b1 = Lib_IntVector_Intrinsics_vec128_load_le(b);
+  print_vector128_64("load_acc2 #b1", b1);
+
+  print_debug_buf8("load_acc2 #b2 src", b + (uint32_t)16U);
   Lib_IntVector_Intrinsics_vec128
   b2 = Lib_IntVector_Intrinsics_vec128_load_le(b + (uint32_t)16U);
+  print_vector128_64("load_acc2 #b2", b2);
+  
   Lib_IntVector_Intrinsics_vec128 lo = Lib_IntVector_Intrinsics_vec128_interleave_low64(b1, b2);
   print_vector128_64("load_acc2 #1", lo);
+  
   Lib_IntVector_Intrinsics_vec128 hi = Lib_IntVector_Intrinsics_vec128_interleave_high64(b1, b2);
   print_vector128_64("load_acc2 #2", hi);
   Lib_IntVector_Intrinsics_vec128
