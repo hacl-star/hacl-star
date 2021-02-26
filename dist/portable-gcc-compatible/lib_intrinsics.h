3335404a90f5be3306027bf43117a61898e88a96
Son Ho <hosonmarc@gmail.com>
2021-02-26 14:19:15 +0100

    Minimize the diff with master by checking out directories from origin/master

--- dist/portable-gcc-compatible/lib_intrinsics.h
+++ dist/portable-gcc-compatible/lib_intrinsics.h
@@ -6,7 +6,7 @@
 #include "config.h"
 #endif
 
-#if defined(SUPPORTS_INTRINSICS)
+#if !defined(BROKEN_INTRINSICS)
 #if defined(_MSC_VER)
 #include <immintrin.h>
 #else
@@ -14,7 +14,7 @@
 #endif
 #endif
 
-#if !defined(SUPPORTS_INTRINSICS)
+#if defined(BROKEN_INTRINSICS)
 
 #include "Hacl_IntTypes_Intrinsics.h"
 
@@ -64,4 +64,4 @@
 
 #endif // GCC < 7.2
 
-#endif // !SUPPORTS_INTRINSICS
+#endif // BROKEN_INTRINSICS
