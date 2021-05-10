a2ceebd9e5619bf37bac928389aab6bcb7f3486b
Dzomo the everest Yak <everbld@microsoft.com>
2021-05-10 15:51:40 +0000

    [CI] regenerate hints and dist

--- dist/mozilla/config.h
+++ dist/mozilla/config.h
@@ -1,3 +1,7 @@
+/*
+ * Mozilla doesn't want to rely on the configure script, so we hardcode the
+ * assumption that AVX and AVX2 are available.
+ */
 #define HACL_CAN_COMPILE_INTRINSICS 1
 #define HACL_CAN_COMPILE_VALE 1
 #define HACL_CAN_COMPILE_INLINE_ASM 1
