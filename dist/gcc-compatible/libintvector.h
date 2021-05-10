2d423c685ea32777e6995ee668901d7686252374
Son Ho <hosonmarc@gmail.com>
2021-05-10 17:07:24 +0200

    Fix some comments

--- dist/gcc-compatible/libintvector.h
+++ dist/gcc-compatible/libintvector.h
@@ -678,7 +678,6 @@ typedef vector128_8 vector128;
 
 /* Small helper to change the endianess of the vector's elements, seen as uint32.
  * Note that we can't use vec_revb. */
-// TODO: remove
 #define Lib_IntVector_Intrinsics_vec128_load_store_switch_endian32(x0) \
   ((vector128)(vec_perm((vector128_8)(x0), (vector128_8) {},            \
   (vector128_8){3,2,1,0,7,6,5,4,11,10,9,8,15,14,13,12})))
@@ -695,13 +694,13 @@ typedef vector128_8 vector128;
 
 #define Lib_IntVector_Intrinsics_vec128_load32_le(x)              \
     ((vector128_32)(__builtin_s390_vlbr((vector128_32)x)))
-//  ((vector128) Lib_IntVector_Intrinsics_vec128_load_store_switch_endian32( \
-//   ((vector128_8)vec_load_len((const uint8_t*)(x), 16))))
+/*  ((vector128) Lib_IntVector_Intrinsics_vec128_load_store_switch_endian32( \
+    ((vector128_8)vec_load_len((const uint8_t*)(x), 16)))) */
 
 #define Lib_IntVector_Intrinsics_vec128_load64_le(x)              \
     ((vector128_64)(__builtin_s390_vlbr((vector128_64)x)))
-//  ((vector128) Lib_IntVector_Intrinsics_vec128_load_store_switch_endian64( \
-//   ((vector128_8)vec_load_len((const uint8_t*)(x), 16))))
+/*  ((vector128) Lib_IntVector_Intrinsics_vec128_load_store_switch_endian64( \
+    ((vector128_8)vec_load_len((const uint8_t*)(x), 16)))) */
 
 #define Lib_IntVector_Intrinsics_vec128_store32_le(x0, x1)             \
    (vec_store_len(((vector128_8)Lib_IntVector_Intrinsics_vec128_load_store_switch_endian32(x1)), \
