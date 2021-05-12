531436c57037150a4abaf899e454e0afec30ad95
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-05-12 09:26:23 +0200

    fix ci

--- .ci/script.sh
+++ .ci/script.sh
@@ -24,7 +24,7 @@ if [[ $ARM_CROSS_CI == "aarch64-none-linux-gnu" ]]; then
 fi
 
 # Cross compile for aarch64-apple-darwin. Build only.
-if [ -z "$CROSS_CI" ]; then
+if [ ! -z "$CROSS_CI" ]; then
   pushd dist/gcc-compatible
   rm -rf *.o *.d libevercrypt.a
   ./configure -target $CROSS_CI --disable-ocaml
