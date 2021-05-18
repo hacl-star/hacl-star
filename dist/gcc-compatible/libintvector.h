bf892723d031adba93b1b0ae1c5d419706845bc8
Son Ho <hosonmarc@gmail.com>
2021-05-18 08:58:07 +0200

    Use builtings other than vec_vlbr{g,f}

--- dist/gcc-compatible/libintvector.h
+++ dist/gcc-compatible/libintvector.h
@@ -696,7 +696,7 @@ typedef vector128_8 vector128;
 static inline
 vector128 Lib_IntVector_Intrinsics_vec128_load32_le(const uint8_t *x0) {
     vector128 x1;
-    vec_vstbrf(&x1, x0);
+    __builtin_s390_vlbrf(&x1, x0);
     return x1;
 }
 
@@ -708,7 +708,7 @@ vector128 Lib_IntVector_Intrinsics_vec128_load32_le(const uint8_t *x0) {
 static inline
 vector128 Lib_IntVector_Intrinsics_vec128_load64_le(const uint8_t *x0) {
     vector128 x1;
-    vec_vstbrg(&x1, x0);
+    __builtin_s390_vlbrg(&x1, x0);
     return x1;
 }
 
