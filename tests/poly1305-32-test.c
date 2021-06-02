3b78d6563b61d1267b48ff7417bc438023bff924
Son Ho <hosonmarc@gmail.com>
2021-06-02 09:37:42 +0200

    Remove the dependency to libsodium in the tests

--- tests/poly1305-32-test.c
+++ tests/poly1305-32-test.c
@@ -8,8 +8,6 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <time.h>
-//#include <openssl/poly1305.h>
-#include <sodium.h>
 
 #include "test_helpers.h"
 
@@ -21,13 +19,6 @@
 #define ROUNDS 100000
 #define SIZE   16384
 
-void sodium_poly1305_mac(uint8_t *tag, uint32_t len, uint8_t *text, uint8_t *key){
-    crypto_onetimeauth_poly1305_state ctx;
-    crypto_onetimeauth_poly1305_init(&ctx, key);
-    crypto_onetimeauth_poly1305_update(&ctx, text, len);
-    crypto_onetimeauth_poly1305_final(&ctx, tag);
-}
-
 bool print_result(uint8_t* comp, uint8_t* exp) {
   return compare_and_print(16, comp, exp);
 }
@@ -38,10 +29,6 @@ bool print_test(int in_len, uint8_t* in, uint8_t* key, uint8_t* exp){
   Hacl_Poly1305_32_poly1305_mac(comp,in_len,in,key);
   printf("Poly1305 (32-bit) Result:\n");
   bool ok = print_result(comp, exp);
-
-  sodium_poly1305_mac(comp,in_len,in,key);
-  printf("Libsodium Poly1305 Result:\n");
-  ok = print_result(comp, exp);
 
   return ok;
 }
@@ -78,20 +65,8 @@ int main() {
   clock_t tdiff1 = t2 - t1;
   cycles cdiff1 = b - a;
 
-  t1 = clock();
-  a = cpucycles_begin();
-  for (int j = 0; j < ROUNDS; j++) {
-    sodium_poly1305_mac(tag,SIZE,plain,key);
-    res ^= tag[0] ^ tag[15];
-  }
-  b = cpucycles_end();
-  t2 = clock();
-  clock_t tdiff2 = t2 - t1;
-  cycles cdiff2 = b - a;
-
   uint64_t count = ROUNDS * SIZE;
   printf("Poly1305 (32-bit) PERF: %d\n",(int)res); print_time(count,tdiff1,cdiff1);
-  printf("Libsodium Poly1305 PERF: %d\n",(int)res); print_time(count,tdiff2,cdiff2);
 
   if (ok) return EXIT_SUCCESS;
   else return EXIT_FAILURE;
