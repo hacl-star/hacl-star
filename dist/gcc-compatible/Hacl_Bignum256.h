c0f2d29e67150454d0d53c863529f22f7c5264bd
marina polubelova <marina@welles.paris.inria.fr>
2021-07-13 22:32:02 +0200

    refresh dist

--- dist/gcc-compatible/Hacl_Bignum256.h
+++ dist/gcc-compatible/Hacl_Bignum256.h
@@ -82,6 +82,30 @@ Write `a - b mod 2^256` in `res`.
 uint64_t Hacl_Bignum256_sub(uint64_t *a, uint64_t *b, uint64_t *res);
 
 /*
+Write `(a + b) mod n` in `res`.
+
+  The arguments a, b, n and the outparam res are meant to be 256-bit bignums, i.e. uint64_t[4].
+
+  Before calling this function, the caller will need to ensure that the following
+  preconditions are observed.
+  • a < n
+  • b < n
+*/
+void Hacl_Bignum256_add_mod(uint64_t *n, uint64_t *a, uint64_t *b, uint64_t *res);
+
+/*
+Write `(a - b) mod n` in `res`.
+
+  The arguments a, b, n and the outparam res are meant to be 256-bit bignums, i.e. uint64_t[4].
+
+  Before calling this function, the caller will need to ensure that the following
+  preconditions are observed.
+  • a < n
+  • b < n
+*/
+void Hacl_Bignum256_sub_mod(uint64_t *n, uint64_t *a, uint64_t *b, uint64_t *res);
+
+/*
 Write `a * b` in `res`.
 
   The arguments a and b are meant to be 256-bit bignums, i.e. uint64_t[4].
@@ -106,7 +130,7 @@ Write `a mod n` in `res`.
   The function returns false if any of the following preconditions are violated,
   true otherwise.
    • 1 < n
-   • n % 2 = 1 
+   • n % 2 = 1
 */
 bool Hacl_Bignum256_mod(uint64_t *n, uint64_t *a, uint64_t *res);
 
@@ -128,7 +152,7 @@ Write `a ^ b mod n` in `res`.
    • n % 2 = 1
    • 1 < n
    • b < pow2 bBits
-   • a < n 
+   • a < n
 */
 bool
 Hacl_Bignum256_mod_exp_vartime(
@@ -157,7 +181,7 @@ Write `a ^ b mod n` in `res`.
    • n % 2 = 1
    • 1 < n
    • b < pow2 bBits
-   • a < n 
+   • a < n
 */
 bool
 Hacl_Bignum256_mod_exp_consttime(
@@ -181,7 +205,7 @@ Write `a ^ (-1) mod n` in `res`.
   • n % 2 = 1
   • 1 < n
   • 0 < a
-  • a < n 
+  • a < n
 */
 bool Hacl_Bignum256_mod_inv_prime_vartime(uint64_t *n, uint64_t *a, uint64_t *res);
 
@@ -253,7 +277,7 @@ Write `a ^ b mod n` in `res`.
   Before calling this function, the caller will need to ensure that the following
   preconditions are observed.
   • b < pow2 bBits
-  • a < n 
+  • a < n
 */
 void
 Hacl_Bignum256_mod_exp_vartime_precomp(
@@ -281,7 +305,7 @@ Write `a ^ b mod n` in `res`.
   Before calling this function, the caller will need to ensure that the following
   preconditions are observed.
   • b < pow2 bBits
-  • a < n 
+  • a < n
 */
 void
 Hacl_Bignum256_mod_exp_consttime_precomp(
@@ -302,7 +326,7 @@ Write `a ^ (-1) mod n` in `res`.
   preconditions are observed.
   • n is a prime
   • 0 < a
-  • a < n 
+  • a < n
 */
 void
 Hacl_Bignum256_mod_inv_prime_vartime_precomp(
@@ -366,10 +390,18 @@ void Hacl_Bignum256_bn_to_bytes_le(uint64_t *b, uint8_t *res);
 
 
 /*
-Returns 2 ^ 64 - 1 if and only if the argument a is strictly less than the argument b,
- otherwise returns 0.
+Returns 2^64 - 1 if a < b, otherwise returns 0.
+
+ The arguments a and b are meant to be 256-bit bignums, i.e. uint64_t[4].
 */
 uint64_t Hacl_Bignum256_lt_mask(uint64_t *a, uint64_t *b);
+
+/*
+Returns 2^64 - 1 if a = b, otherwise returns 0.
+
+ The arguments a and b are meant to be 256-bit bignums, i.e. uint64_t[4].
+*/
+uint64_t Hacl_Bignum256_eq_mask(uint64_t *a, uint64_t *b);
 
 #if defined(__cplusplus)
 }
