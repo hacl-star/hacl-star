a34da706afd1d7297c626e36fe5a4b652f783f1f
Anthony R Johnston <ajohnston@optoro.com>
2021-07-20 13:02:29 -0400

    Use parameter for make executable

--- .ci/script.sh
+++ .ci/script.sh
@@ -3,6 +3,13 @@
 set -e
 set -o pipefail
 
+if [[ `uname` == "FreeBSD" ]]
+then
+  MAKE=$(which gmake)
+else
+  MAKE=$(which make)
+fi
+
 if [[ $OS == "Windows_NT" ]]; then
   # The usual issue of return codes not being forwarded.
   .ci/script.bat 2>&1 | tee log
@@ -18,7 +25,7 @@ if [[ $ARM_CROSS_CI == "aarch64-none-linux-gnu" ]]; then
   pushd dist/gcc-compatible
   export TOOLCHAIN=$PWD/../../gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
   ./configure -target aarch64-none-linux-gnu
-  make -j
+  $MAKE -j
   popd
   exit 0
 fi
@@ -28,7 +35,7 @@ if [ ! -z "$CROSS_CI" ]; then
   pushd dist/gcc-compatible
   rm -rf *.o *.d libevercrypt.a
   ./configure -target $CROSS_CI --disable-ocaml
-  make -j libevercrypt.a
+  $MAKE -j libevercrypt.a
   popd
   exit 0
 fi
@@ -37,7 +44,7 @@ if [[ $TARGET == "IA32" ]]; then
   # Test 32-bit build; Tests don't work here yet.
   pushd dist/gcc-compatible
   ./configure -target ia32
-  make -j
+  $MAKE -j
   popd
   exit 0
 fi
@@ -50,22 +57,22 @@ for p in /usr/local/Cellar/openssl@1.1/*; do export CFLAGS="-I$p/include/"; expo
 # Most likely running without OCaml -- need to configure
 pushd dist/gcc-compatible
 ./configure
-make -j
+$MAKE -j
 popd
 
-make -C tests -j test
+$MAKE -C tests -j test
 
 # Extracted C tests -- need full kremlib, don't work on ARM because of
 # intrinsics for x86 in cpu cycle count routines in testlib.c
 pushd dist/test/c/
 git clone https://github.com/fstarlang/kremlin --depth 10
 export KREMLIN_HOME=$(pwd)/kremlin
-make -C kremlin/kremlib/dist/generic -f Makefile.basic -j
-make -j -k
+$MAKE -C kremlin/kremlib/dist/generic -f Makefile.basic -j
+$MAKE -j -k
 popd
 
 if which opam; then
-  make -C dist/gcc-compatible install-hacl-star-raw
+  $MAKE -C dist/gcc-compatible install-hacl-star-raw
   pushd bindings/ocaml
   dune build
   dune runtest
