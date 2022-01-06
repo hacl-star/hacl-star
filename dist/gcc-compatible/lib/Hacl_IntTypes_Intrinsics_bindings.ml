ac1271d1d1c4f0eed7542c4db162ad35d30f00a3
Dzomo, the Everest Yak <everbld@microsoft.com>
2022-01-06 08:39:25 +0000

    [CI] regenerate hints and dist

--- dist/gcc-compatible/lib/Hacl_IntTypes_Intrinsics_bindings.ml
+++ dist/gcc-compatible/lib/Hacl_IntTypes_Intrinsics_bindings.ml
@@ -7,16 +7,16 @@ module Bindings(F:Cstubs.FOREIGN) =
         (uint32_t @->
            (uint32_t @->
               (uint32_t @-> ((ptr uint32_t) @-> (returning uint32_t)))))
-    let hacl_IntTypes_Intrinsics_add_carry_u64 =
-      foreign "Hacl_IntTypes_Intrinsics_add_carry_u64"
-        (uint64_t @->
-           (uint64_t @->
-              (uint64_t @-> ((ptr uint64_t) @-> (returning uint64_t)))))
     let hacl_IntTypes_Intrinsics_sub_borrow_u32 =
       foreign "Hacl_IntTypes_Intrinsics_sub_borrow_u32"
         (uint32_t @->
            (uint32_t @->
               (uint32_t @-> ((ptr uint32_t) @-> (returning uint32_t)))))
+    let hacl_IntTypes_Intrinsics_add_carry_u64 =
+      foreign "Hacl_IntTypes_Intrinsics_add_carry_u64"
+        (uint64_t @->
+           (uint64_t @->
+              (uint64_t @-> ((ptr uint64_t) @-> (returning uint64_t)))))
     let hacl_IntTypes_Intrinsics_sub_borrow_u64 =
       foreign "Hacl_IntTypes_Intrinsics_sub_borrow_u64"
         (uint64_t @->
--- dist/mozilla/Hacl_Kremlib.h
+++ dist/mozilla/Hacl_Kremlib.h
@@ -37,10 +37,6 @@ extern "C" {
 
 
 
-static inline uint32_t FStar_UInt32_eq_mask(uint32_t a, uint32_t b);
-
-static inline uint32_t FStar_UInt32_gte_mask(uint32_t a, uint32_t b);
-
 static inline uint8_t FStar_UInt8_eq_mask(uint8_t a, uint8_t b);
 
 static inline uint64_t FStar_UInt64_eq_mask(uint64_t a, uint64_t b);
@@ -49,6 +45,12 @@ static inline uint64_t FStar_UInt64_gte_mask(uint64_t a, uint64_t b);
 
 static inline FStar_UInt128_uint128
 FStar_UInt128_add(FStar_UInt128_uint128 a, FStar_UInt128_uint128 b);
+
+static inline FStar_UInt128_uint128
+FStar_UInt128_add_mod(FStar_UInt128_uint128 a, FStar_UInt128_uint128 b);
+
+static inline FStar_UInt128_uint128
+FStar_UInt128_sub_mod(FStar_UInt128_uint128 a, FStar_UInt128_uint128 b);
 
 static inline FStar_UInt128_uint128
 FStar_UInt128_shift_right(FStar_UInt128_uint128 a, uint32_t s);
