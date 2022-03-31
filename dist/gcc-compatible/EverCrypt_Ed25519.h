b45ff74a80672cb68bb8724cad93fa2181bfb1e8
Marina Polubelova <polubelovam@gmail.com>
2022-03-31 20:26:46 +0200

    consistency

--- dist/gcc-compatible/EverCrypt_Ed25519.h
+++ dist/gcc-compatible/EverCrypt_Ed25519.h
@@ -38,16 +38,33 @@ extern "C" {
 #include "Hacl_Ed25519.h"
 #include "evercrypt_targetconfig.h"
 #include "libintvector.h"
-void EverCrypt_Ed25519_secret_to_public(uint8_t *output, uint8_t *secret);
+void EverCrypt_Ed25519_secret_to_public(uint8_t *public_key, uint8_t *private_key);
 
-void EverCrypt_Ed25519_expand_keys(uint8_t *ks, uint8_t *secret);
+void EverCrypt_Ed25519_expand_keys(uint8_t *expanded_keys, uint8_t *private_key);
 
 void
-EverCrypt_Ed25519_sign_expanded(uint8_t *signature, uint8_t *ks, uint32_t len, uint8_t *msg);
+EverCrypt_Ed25519_sign_expanded(
+  uint8_t *signature,
+  uint8_t *expanded_keys,
+  uint32_t msg_len,
+  uint8_t *msg
+);
 
-void EverCrypt_Ed25519_sign(uint8_t *signature, uint8_t *secret, uint32_t len, uint8_t *msg);
+void
+EverCrypt_Ed25519_sign(
+  uint8_t *signature,
+  uint8_t *private_key,
+  uint32_t msg_len,
+  uint8_t *msg
+);
 
-bool EverCrypt_Ed25519_verify(uint8_t *pubkey, uint32_t len, uint8_t *msg, uint8_t *signature);
+bool
+EverCrypt_Ed25519_verify(
+  uint8_t *public_key,
+  uint32_t msg_len,
+  uint8_t *msg,
+  uint8_t *signature
+);
 
 #if defined(__cplusplus)
 }
--- providers/evercrypt/fst/EverCrypt.Ed25519.fst
+++ providers/evercrypt/fst/EverCrypt.Ed25519.fst
@@ -2,17 +2,17 @@ module EverCrypt.Ed25519
 
 /// For now, only one implementation... to be improved in the future.
 
-let secret_to_public output secret =
-  Hacl.Ed25519.secret_to_public output secret
+let secret_to_public public_key private_key =
+  Hacl.Ed25519.secret_to_public public_key private_key
 
-let expand_keys ks secret =
-  Hacl.Ed25519.expand_keys ks secret
+let expand_keys expanded_keys private_key =
+  Hacl.Ed25519.expand_keys expanded_keys private_key
 
-let sign_expanded signature ks len msg =
-  Hacl.Ed25519.sign_expanded signature ks len msg
+let sign_expanded signature expanded_keys msg_len msg =
+  Hacl.Ed25519.sign_expanded signature expanded_keys msg_len msg
 
-let sign signature secret len msg =
-  Hacl.Ed25519.sign signature secret len msg
+let sign signature private_key msg_len msg =
+  Hacl.Ed25519.sign signature private_key msg_len msg
 
-let verify pubkey len msg signature =
-  Hacl.Ed25519.verify pubkey len msg signature
+let verify public_key msg_len msg signature =
+  Hacl.Ed25519.verify public_key msg_len msg signature
