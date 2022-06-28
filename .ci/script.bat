74879a28dc03c81befb6b5b3a430a81486a77a79
Jonathan Protzenko <protz@microsoft.com>
2022-06-28 13:27:44 -0700

    Trying to figure out how to do  on Windows

--- .ci/script.bat
+++ .ci/script.bat
@@ -1,7 +1,7 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" -test
 cd dist/msvc-compatible
-echo > config.h
+type nul > config.h
 cl *.c /I ../karamel/include /I . /I ../karamel/krmllib/dist/minimal /c /DHACL_CAN_COMPILE_INTRINSICS /DHACL_CAN_COMPILE_VALE /DHACL_CAN_COMPILE_VEC128 /DHACL_CAN_COMPILE_VEC256 || goto :error
 for /F %%i in ('dir /b *-x86_64-msvc.asm') do (
   ml64 /c %%i || goto :error
