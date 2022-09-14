59b71c5d9233ac4004e724ec1df9ca14d9349f6b
Marina Polubelova <marina.polubelova@nomadic-labs.com>
2022-09-14 19:22:23 +0200

    add precomp_table_g for w = 15

---
+++ code/k256/dist (wNAF + precomp15)/Hacl_Krmllib.h
@@ -0,0 +1,56 @@
+/* MIT License
+ *
+ * Copyright (c) 2016-2020 INRIA, CMU and Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+#ifndef __Hacl_Krmllib_H
+#define __Hacl_Krmllib_H
+#include <string.h>
+#include "krml/internal/types.h"
+#include "krml/lowstar_endianness.h"
+
+
+
+#include "lib_intrinsics.h"
+static inline uint64_t FStar_UInt64_eq_mask(uint64_t a, uint64_t b);
+
+static inline uint64_t FStar_UInt64_gte_mask(uint64_t a, uint64_t b);
+
+static inline uint128_t FStar_UInt128_add(uint128_t a, uint128_t b);
+
+static inline uint128_t FStar_UInt128_add_mod(uint128_t a, uint128_t b);
+
+static inline uint128_t FStar_UInt128_shift_left(uint128_t a, uint32_t s);
+
+static inline uint128_t FStar_UInt128_shift_right(uint128_t a, uint32_t s);
+
+static inline uint128_t FStar_UInt128_uint64_to_uint128(uint64_t a);
+
+static inline uint64_t FStar_UInt128_uint128_to_uint64(uint128_t a);
+
+static inline uint128_t FStar_UInt128_mul_wide(uint64_t x, uint64_t y);
+
+static inline void store128_be(uint8_t *x0, uint128_t x1);
+
+
+#define __Hacl_Krmllib_H_DEFINED
+#endif
