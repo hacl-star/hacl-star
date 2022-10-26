632f5c640d3012087835a56533238afb3025cc38
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-10-26 08:00:00 +0200

    move nix derivation to the main repository

--- flake.nix
+++ flake.nix
@@ -14,10 +14,31 @@
       url = "github:hacl-star/hacl-nix";
       inputs.fstar-src.follows = "fstar-src";
       inputs.karamel-src.follows = "karamel-src";
-      # `/` means `self` (see NixOS/nix#4931)
-      inputs.hacl-src.follows = "/";
     };
+    nixpkgs.follows = "hacl-nix/nixpkgs";
+    flake-utils.follows = "hacl-nix/flake-utils";
   };
 
-  outputs = { hacl-nix, ... }: hacl-nix;
+  outputs = { hacl-nix, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_12;
+        hacl = pkgs.callPackage ./. {
+          inherit (hacl-nix.packages.${system}) z3 fstar karamel vale;
+          inherit ocamlPackages;
+        };
+      in {
+        packages = {
+          inherit hacl;
+          default = hacl;
+        };
+        hydraJobs = {
+          inherit hacl;
+          hacl-build-products = hacl.passthru.build-products;
+          hacl-stats = hacl.passthru.stats;
+          hacl-dist-compare = hacl.passthru.dist-compare;
+          hacl-dist-list = hacl.passthru.dist-list;
+        };
+      });
 }
