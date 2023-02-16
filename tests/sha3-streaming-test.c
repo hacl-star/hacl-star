67701d7b72274891cb636d88dd3e1e4c1dd565e3
Jonathan Protzenko <protz@microsoft.com>
2023-02-16 13:29:00 -0800

    Fix tests

--- tests/sha3-streaming-test.c
+++ tests/sha3-streaming-test.c
@@ -1,4 +1,4 @@
-#include "Hacl_Streaming_SHA3.h"
+#include "Hacl_Hash_SHA3.h"
 #include <assert.h>
 
 const uint8_t expected_hash[] = { 0x64, 0x4b, 0xcc, 0x7e, 0x56, 0x43, 0x73,
@@ -10,7 +10,7 @@ const uint8_t expected_hash[] = { 0x64, 0x4b, 0xcc, 0x7e, 0x56, 0x43, 0x73,
 int
 main()
 {
-  Hacl_Streaming_SHA3_state_sha3_256* s = Hacl_Streaming_SHA3_create_in_256();
+  Hacl_Streaming_SHA3_state_256* s = Hacl_Streaming_SHA3_create_in_256();
   assert(Hacl_Streaming_SHA3_update_256(s, (uint8_t*)"hello world", 11) == 0);
   uint8_t hash[32] = { 0 };
   Hacl_Streaming_SHA3_finish_256(s, hash);
