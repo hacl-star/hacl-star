9e12b80ee574b21f5dc0f2e50f0a9f98e1692f81
Jonathan Protzenko <protz@microsoft.com>
2023-10-05 15:06:04 -0700

    Code-gen improvements from krml/protz_ignore_ifdef

--- dist/karamel/include/krml/internal/target.h
+++ dist/karamel/include/krml/internal/target.h
@@ -57,6 +57,12 @@
 #  define KRML_HOST_IGNORE(x) (void)(x)
 #endif
 
+#ifndef KRML_MAYBE_UNUSED
+#  if defined(__GNUC__)
+#    define KRML_MAYBE_UNUSED __attribute__((unused))
+#  endif
+#endif
+
 #ifndef KRML_NOINLINE
 #  if defined(_MSC_VER)
 #    define KRML_NOINLINE __declspec(noinline)
