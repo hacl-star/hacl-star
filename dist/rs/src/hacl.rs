d7be9e04974489b2d57bf5e65860d5f8d9347d0a
Jonathan Protzenko <protz@microsoft.com>
2023-11-13 17:05:44 -0800

    Comment out stuff that needs further rewrites

--- dist/rs/src/hacl.rs
+++ dist/rs/src/hacl.rs
@@ -5,8 +5,9 @@ pub mod poly1305_32;
 pub mod chacha20poly1305_32;
 pub mod bignum25519_51;
 pub mod curve25519_51;
-pub mod bignum;
-pub mod bignum_base;
+// BIGNUM: lots of borrow errors (but type-checks otherwise).
+// pub mod bignum_base;
+// pub mod bignum;
 // pub mod bignum32;
 // pub mod bignum64;
 // pub mod bignum256;
