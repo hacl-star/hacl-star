b0f7ac40918b271f89904a6b6848acb4147ec065
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2024-03-15 19:09:21 +0100

    Add Rust tests for ed25519

--- dist/rs/src/lib.rs
+++ dist/rs/src/lib.rs
@@ -7,12 +7,18 @@ pub mod lib {
 pub mod hacl;
 
 pub mod test {
+  // Blake2 currently panicks with a substract with overflow
+  // pub mod blake2;
   pub mod chacha20;
   pub mod chachapoly;
   pub mod curve;
+  // Ed25519 currently does not borrow-check
+  // pub mod ed25519;
   // FFDHE currently panicks with a substract with overflow in bignum
   // pub mod ffdhe;
   pub mod poly1305;
+  // RSAPSS currently panicks with a substract with overflow in bignum
+  // pub mod rsapss;
   pub mod salsa20;
   pub mod sha2;
 }
