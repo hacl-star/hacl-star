7ba487de6d67a1a78d68cb7fc0a234938d7177d1
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2024-06-18 10:31:05 +0200

    Temporarily disable blake and hmac due to extraction issues

--- dist/rs/src/lib.rs
+++ dist/rs/src/lib.rs
@@ -8,13 +8,13 @@ pub mod hacl;
 
 pub mod test {
    pub mod bignum4096;
-   pub mod blake2;
+//   pub mod blake2;
    pub mod chacha20;
    pub mod chachapoly;
    pub mod curve;
    pub mod ecdhp256;
    pub mod ed25519;
-   pub mod hmac;
+//    pub mod hmac;
    pub mod ffdhe;
    pub mod p256;
    pub mod poly1305;
