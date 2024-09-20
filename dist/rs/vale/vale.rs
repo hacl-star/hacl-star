0c21c5f77a0580015f856fce544979fc8bee3810
Jonathan Protzenko <protz@microsoft.com>
2024-09-20 10:49:22 -0700

    Cleanup files that are no longer generated

--- dist/rs/vale/vale.rs
+++
@@ -1,8 +0,0 @@
-#![allow(non_snake_case)]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(unused_assignments)]
-#![allow(unreachable_patterns)]
-#![allow(const_item_mutation)]
-
-type als_ret = u64;
