8b1780b55187d2c291eecf751d2910f33ad66ddb
Jonathan Protzenko <protz@microsoft.com>
2024-11-01 11:41:16 -0700

    Refresh C code

--- dist/gcc-compatible/lib/Hacl_HMAC_bindings.ml
+++ dist/gcc-compatible/lib/Hacl_HMAC_bindings.ml
@@ -2,8 +2,18 @@ open Ctypes
 module Bindings(F:Cstubs.FOREIGN) =
   struct
     open F
+    let hacl_HMAC_compute_md5 =
+      foreign "Hacl_HMAC_compute_md5"
+        (ocaml_bytes @->
+           (ocaml_bytes @->
+              (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
     let hacl_HMAC_compute_sha1 =
       foreign "Hacl_HMAC_compute_sha1"
+        (ocaml_bytes @->
+           (ocaml_bytes @->
+              (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
+    let hacl_HMAC_compute_sha2_224 =
+      foreign "Hacl_HMAC_compute_sha2_224"
         (ocaml_bytes @->
            (ocaml_bytes @->
               (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
@@ -19,6 +29,26 @@ module Bindings(F:Cstubs.FOREIGN) =
               (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
     let hacl_HMAC_compute_sha2_512 =
       foreign "Hacl_HMAC_compute_sha2_512"
+        (ocaml_bytes @->
+           (ocaml_bytes @->
+              (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
+    let hacl_HMAC_compute_sha3_224 =
+      foreign "Hacl_HMAC_compute_sha3_224"
+        (ocaml_bytes @->
+           (ocaml_bytes @->
+              (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
+    let hacl_HMAC_compute_sha3_256 =
+      foreign "Hacl_HMAC_compute_sha3_256"
+        (ocaml_bytes @->
+           (ocaml_bytes @->
+              (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
+    let hacl_HMAC_compute_sha3_384 =
+      foreign "Hacl_HMAC_compute_sha3_384"
+        (ocaml_bytes @->
+           (ocaml_bytes @->
+              (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
+    let hacl_HMAC_compute_sha3_512 =
+      foreign "Hacl_HMAC_compute_sha3_512"
         (ocaml_bytes @->
            (ocaml_bytes @->
               (uint32_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))))
