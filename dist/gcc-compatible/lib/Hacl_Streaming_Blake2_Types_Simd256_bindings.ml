e36de87eed4288950c818b86213faa9b4fb644f7
Jonathan Protzenko <protz@microsoft.com>
2025-03-04 11:51:36 -0800

    Missing files

---
+++ dist/gcc-compatible/lib/Hacl_Streaming_Blake2_Types_Simd256_bindings.ml
@@ -0,0 +1,14 @@
+open Ctypes
+module Bindings(F:Cstubs.FOREIGN) =
+  struct
+    open F
+    type hacl_Streaming_Blake2_Types_Simd256_block_state_blake2b_256 =
+      [ `hacl_Streaming_Blake2_Types_Simd256_block_state_blake2b_256 ]
+        structure
+    let (hacl_Streaming_Blake2_Types_Simd256_block_state_blake2b_256 :
+      [ `hacl_Streaming_Blake2_Types_Simd256_block_state_blake2b_256 ]
+        structure typ)
+      =
+      structure
+        "Hacl_Streaming_Blake2_Types_Simd256_block_state_blake2b_256_s"
+  end
